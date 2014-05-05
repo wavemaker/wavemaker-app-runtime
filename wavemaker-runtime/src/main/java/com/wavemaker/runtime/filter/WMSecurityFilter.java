37617261ecca0634de8c0b269365d0e64214849e
Uday Shankar <uday.adepu@wavemaker.com>
2014-05-05 12:17:22 +0530

    Fixed security issues related to enabling and disabling.
    Change-Id: Ib18a522158ed1c43d41ddc58e469c27587318d80

--- wavemaker-runtime/src/main/java/com/wavemaker/runtime/filter/WMSecurityFilter.java
+++ wavemaker-runtime/src/main/java/com/wavemaker/runtime/filter/WMSecurityFilter.java
@@ -9,12 +9,19 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.filter.DelegatingFilterProxy;
+
+import com.wavemaker.runtime.WMAppContext;
+import com.wavemaker.runtime.security.WMSecurityConfigStore;
 
 /**
  * @author Uday Shankar
  */
 public class WMSecurityFilter extends DelegatingFilterProxy {
+
+    private Boolean isSecurityEnforced;
 
     private boolean appTestRunMode;
 
@@ -27,13 +34,24 @@ public class WMSecurityFilter extends DelegatingFilterProxy {
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-
-        if (appTestRunMode && "true".equals(((HttpServletRequest) servletRequest).getHeader("skipSecurity"))) {
+        if (!isSecurityEnforced() || (appTestRunMode && "true".equals(((HttpServletRequest) servletRequest).getHeader("skipSecurity")))) {
             // Ignore the DelegatingProxyFilter delegate
             filterChain.doFilter(servletRequest, servletResponse);
         } else {
             // Call the delegate
             super.doFilter(servletRequest, servletResponse, filterChain);
         }
+    }
+
+    private boolean isSecurityEnforced() {
+        if(isSecurityEnforced == null) {
+            try {
+                WMSecurityConfigStore wmSecurityConfigStore = WMAppContext.getInstance().getSpringBean(WMSecurityConfigStore.class);
+                isSecurityEnforced = wmSecurityConfigStore.isEnforceSecurity();
+            } catch (NoSuchBeanDefinitionException e) {
+                isSecurityEnforced = false;
+            }
+        }
+        return isSecurityEnforced;
     }
 }
