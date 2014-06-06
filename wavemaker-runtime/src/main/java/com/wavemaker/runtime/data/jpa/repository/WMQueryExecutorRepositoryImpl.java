79b7e7eec325864ecf39af06b1800e89f9e79a45
GKS <gaurav.sharma@imaginea.com>
2014-06-06 17:57:55 +0530

    Changes for WMQueryExecutorRepositoryImpl and pagination of resultset.
    Change-Id: Id377f63e640b860fffde5a711213d21b094e5683

--- wavemaker-runtime/src/main/java/com/wavemaker/runtime/data/jpa/repository/WMQueryExecutorRepositoryImpl.java
+++ wavemaker-runtime/src/main/java/com/wavemaker/runtime/data/jpa/repository/WMQueryExecutorRepositoryImpl.java
@@ -2,123 +2,132 @@ package com.wavemaker.runtime.data.jpa.repository;
 
 import com.wavemaker.runtime.data.jpa.exception.QueryParameterMismatchException;
 import com.wavemaker.runtime.data.jpa.util.QueryHelper;
+import java.util.List;
+import java.util.Map;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Parameter;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import org.apache.commons.lang.reflect.ConstructorUtils;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.NoRepositoryBean;
 
-import javax.persistence.EntityManager;
-import javax.persistence.Parameter;
-import javax.persistence.Query;
-import java.math.BigInteger;
-import java.util.List;
-import java.util.Map;
-
-//@Repository
+@NoRepositoryBean
 public class WMQueryExecutorRepositoryImpl implements WMQueryExecutorRepository {
 
-	//@PersistenceContext
-	private EntityManager entityManager;
+    @PersistenceContext
+    private EntityManager entityManager;
 
-	@Override
-	public Page<Object> executeNativeQuery(com.wavemaker.runtime.data.model.Query queryInput,
-			Map<String, Object> params, Pageable pageable)
-			throws QueryParameterMismatchException {
-		
-		String queryStr = queryInput.getQuery(); 
-		Query query = getNativeQuery(queryStr, params);
-		Long count = getNativeQueryCount(queryStr, params);
+    @Override
+    public Page<Object> executeNativeQuery(com.wavemaker.runtime.data.model.Query queryInput,
+                                           Map<String, Object> params, Pageable pageable)
+            throws QueryParameterMismatchException {
+        String queryStr = queryInput.getQuery();
 
-		return getPageableQueryResult(pageable, query, count);
-	}
+        Query query = getNativeQuery(queryStr, params);
+        Long count = getCount(queryStr, params, true);
 
-	private Long getNativeQueryCount(String queryStr, Map<String, Object> params)
-			throws QueryParameterMismatchException {
-		String strQuery = QueryHelper.getCountNativeQuery(queryStr, params);
-		Query query = entityManager.createNativeQuery(strQuery);
-		Long count = ((BigInteger) query.getSingleResult()).longValue();
-		return count;
-	}
+        return getPageableQueryResult(pageable, query, count);
+    }
 
-	@SuppressWarnings("unchecked")
-	private Page<Object> getPageableQueryResult(Pageable pageable, Query query,
-			Long count) {
-		query.setFirstResult(pageable.getOffset());
-		query.setMaxResults(pageable.getPageSize());
-		List<Object> resultList = query.getResultList();
-		return new PageImpl<Object>(resultList, pageable, count);
-	}
+    private Long getCount(String queryStr, Map<String, Object> params, boolean isNative)
+            throws QueryParameterMismatchException {
+        String strQuery = QueryHelper.getCountQuery(queryStr, params);
+        Query query=null;
+        if(isNative)
+            query = entityManager.createNativeQuery(strQuery);
+        else
+            query=entityManager.createQuery(strQuery);
 
-	@Override
-	public Page<Object> executeJPQLQuery(com.wavemaker.runtime.data.model.Query queryInput,
-			Map<String, Object> params, Pageable pageable)
-			throws QueryParameterMismatchException {
-		
-		String queryStr = queryInput.getQuery();
-		Query query = getJPQLQuery(queryStr, params);
+        Long count = ((Number) query.getSingleResult()).longValue();
+        return count;
+    }
 
-		return getPageableQueryResult(pageable, query, 100L);// This is
-																// temporary, It
-																// needed to be
-																// changed.
+    @SuppressWarnings("unchecked")
+    private Page<Object> getPageableQueryResult(Pageable pageable, Query query,
+                                                Long count) {
+        query.setFirstResult(pageable.getOffset());
+        query.setMaxResults(pageable.getPageSize());
+        List<Object> resultList = query.getResultList();
+        return new PageImpl<Object>(resultList, pageable, count);
+    }
 
-	}
+    @Override
+    public Page<Object> executeJPQLQuery(com.wavemaker.runtime.data.model.Query queryInput,
+                                         Map<String, Object> params, Pageable pageable)
+            throws QueryParameterMismatchException {
+        String queryStr = queryInput.getQuery();
+        Query query = getJPQLQuery(queryStr, params);
 
-	private Query getJPQLQuery(String queryStr, Map<String, Object> params)
-			throws QueryParameterMismatchException {
-		Query query = entityManager.createQuery(queryStr);
-		if (!params.isEmpty()) {
-			setJPQLQueryParams(query, params);
-		}
-		return query;
-	}
+        Long count = getCount(queryStr, params, false);
 
-	private Query getNativeQuery(String queryStr, Map<String, Object> params) {
-		Query query = entityManager.createNativeQuery(queryStr);
-		if (!params.isEmpty()) {
-			setQueryParams(query, params);
-		}
-		return query;
-	}
+        return getPageableQueryResult(pageable, query, count);
 
-	private void setQueryParams(Query query, Map<String, Object> params) {
-		if (!params.isEmpty()) {
-			for (Object key : params.keySet()) {
-				String queryParamName = key.toString();
-				Object queryParamValue = params.get(key);
-				query.setParameter(queryParamName, queryParamValue);
-			}
-		}
+//		return getPageableQueryResult(pageable, query, 100L);// This is
+        // temporary, It
+        // needed to be
+        // changed.
 
-	}
+    }
 
-	private void setJPQLQueryParams(Query query, Map<String, Object> params)
-			throws QueryParameterMismatchException {
-		if (!params.isEmpty()) {
-			for (Object key : params.keySet()) {
-				Object object = null;
-				String queryParamName = key.toString();
-				Object queryParamValue = params.get(key);
-				Parameter<?> parameter = query.getParameter(queryParamName);
-				if (parameter == null)
-					throw new QueryParameterMismatchException("Parameter "
-							+ queryParamName
-							+ " does not exist in named query.");
-				Class<?> parameterType = parameter.getParameterType();
-				try {
-					object = ConstructorUtils.invokeExactConstructor(
-							parameterType, queryParamValue);
-				} catch (Exception exception) {
-					throw new QueryParameterMismatchException(
-							"Parameter "
-									+ queryParamName
-									+ " could not be converted to object of expected query type "
-									+ parameterType, exception);
+    private Query getJPQLQuery(String queryStr, Map<String, Object> params)
+            throws QueryParameterMismatchException {
+        Query query = entityManager.createQuery(queryStr);
+        if (!params.isEmpty()) {
+            setJPQLQueryParams(query, params);
+        }
+        return query;
+    }
 
-				}
-				query.setParameter(queryParamName, object);
-			}
-		}
+    private Query getNativeQuery(String queryStr, Map<String, Object> params) {
+        Query query = entityManager.createNativeQuery(queryStr);
+        if (!params.isEmpty()) {
+            setQueryParams(query, params);
+        }
+        return query;
+    }
 
-	}
+    private void setQueryParams(Query query, Map<String, Object> params) {
+        if (!params.isEmpty()) {
+            for (Object key : params.keySet()) {
+                String queryParamName = key.toString();
+                Object queryParamValue = params.get(key);
+                query.setParameter(queryParamName, queryParamValue);
+            }
+        }
+
+    }
+
+    private void setJPQLQueryParams(Query query, Map<String, Object> params)
+            throws QueryParameterMismatchException {
+        if (!params.isEmpty()) {
+            for (Object key : params.keySet()) {
+                Object object = null;
+                String queryParamName = key.toString();
+                Object queryParamValue = params.get(key);
+                Parameter<?> parameter = query.getParameter(queryParamName);
+                if (parameter == null)
+                    throw new QueryParameterMismatchException("Parameter "
+                            + queryParamName
+                            + " does not exist in named query.");
+                Class<?> parameterType = parameter.getParameterType();
+                try {
+                    object = ConstructorUtils.invokeExactConstructor(
+                            parameterType, queryParamValue);
+                } catch (Exception exception) {
+                    throw new QueryParameterMismatchException(
+                            "Parameter "
+                                    + queryParamName
+                                    + " could not be converted to object of expected query type "
+                                    + parameterType, exception);
+
+                }
+                query.setParameter(queryParamName, object);
+            }
+        }
+
+    }
 }
--- wavemaker-runtime/src/main/java/com/wavemaker/runtime/data/jpa/util/QueryHelper.java
+++ wavemaker-runtime/src/main/java/com/wavemaker/runtime/data/jpa/util/QueryHelper.java
@@ -10,37 +10,37 @@ import com.wavemaker.runtime.data.jpa.exception.QueryParameterMismatchException;
 
 public class QueryHelper {
 
-	private static final String SELECT_COUNT = "Select count(*)";
-	private static final String FROM = " FROM ";
-	
-	private static final Logger LOGGER = LoggerFactory
-			.getLogger(QueryHelper.class);
+    private static final String SELECT_COUNT = "Select count(*)";
+    private static final String FROM = " FROM ";
 
-	public static String getCountNativeQuery(String query, Map<String, Object>params)
-			throws QueryParameterMismatchException {
-		LOGGER.debug("Getting count query for query {} with params {}", query, params);
-		if (params != null) {
-			for (Object key : params.keySet()) {
-				String queryParamName = key.toString();
-				Object queryParamValue = params.get(key);
-				String parameterPlaceholder = new StringBuilder(":").append(
-						queryParamName).toString();
-				if (!query.contains(parameterPlaceholder)) {
-					throw new QueryParameterMismatchException("Parameter "
-							+ queryParamName
-							+ " does not exist in named query.");
-				} else {
-					query = StringUtils.replace(query, parameterPlaceholder,
-							queryParamValue.toString());
-				}
-			}
-		}
-		LOGGER.debug("Got query string after placing params {}", query);
-		int index = StringUtils.indexOfIgnoreCase(query, FROM);
-		String subQuery = query.substring(index, query.length());
-		String countQuery=SELECT_COUNT + subQuery;
-		LOGGER.debug("Got count query string {}", countQuery);
-		return countQuery;
-	}
+    private static final Logger LOGGER = LoggerFactory
+            .getLogger(QueryHelper.class);
+
+    public static String getCountQuery(String query, Map<String, Object> params)
+            throws QueryParameterMismatchException {
+        LOGGER.debug("Getting count query for query {} with params {}", query, params);
+        if (params != null) {
+            for (String key : params.keySet()) {
+                String queryParamName = key.toString();
+                Object queryParamValue = params.get(key);
+                String parameterPlaceholder = new StringBuilder(":").append(
+                        queryParamName).toString();
+                if (!query.contains(parameterPlaceholder)) {
+                    throw new QueryParameterMismatchException("Parameter "
+                            + queryParamName
+                            + " does not exist in named query.");
+                } else {
+                    query = StringUtils.replace(query, parameterPlaceholder,
+                            String.valueOf(queryParamValue));
+                }
+            }
+        }
+        LOGGER.debug("Got query string after placing params {}", query);
+        int index = StringUtils.indexOfIgnoreCase(query, FROM);
+        String subQuery = query.substring(index, query.length());
+        String countQuery=SELECT_COUNT + subQuery;
+        LOGGER.debug("Got count query string {}", countQuery);
+        return countQuery;
+    }
 
 }
