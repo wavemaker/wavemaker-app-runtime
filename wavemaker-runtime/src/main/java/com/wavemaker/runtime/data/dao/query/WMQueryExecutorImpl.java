053e7eb4a4044eb0998d35a01cff39a97b1a13e5
GKS <gaurav.sharma@imaginea.com>
2014-06-12 16:41:21 +0530

    Changes for pagination of QueryExecutor.
    Change-Id: I83e6465b102ae978e3ba59da41d69b6896e725a9

--- wavemaker-runtime/src/main/java/com/wavemaker/runtime/data/dao/query/WMQueryExecutorImpl.java
+++ wavemaker-runtime/src/main/java/com/wavemaker/runtime/data/dao/query/WMQueryExecutorImpl.java
@@ -18,12 +18,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.orm.hibernate4.HibernateTemplate;
-import org.springframework.transaction.annotation.Transactional;
 
 public class WMQueryExecutorImpl implements WMQueryExecutor {
 
     private static final Logger LOGGER = LoggerFactory
             .getLogger(WMQueryExecutorImpl.class);
+    private static final long UNKNOWN_COUNT=-1L;
 
 	private HibernateTemplate template;
 
@@ -39,12 +39,16 @@ public class WMQueryExecutorImpl implements WMQueryExecutor {
 		Session currentSession = template.getSessionFactory().getCurrentSession();
 		
 		Query namedQuery = currentSession.getNamedQuery(queryName);
-
-        setResultTransformer(namedQuery);
-		
-		configureParameters(namedQuery, params);
-		
-		return new PageImpl<Object>(namedQuery.list());
+        QueryHelper.setResultTransformer(namedQuery);
+		QueryHelper.configureParameters(namedQuery, params);
+        if(pageable!=null)
+        {
+            namedQuery.setFirstResult(pageable.getOffset());
+            namedQuery.setMaxResults(pageable.getPageSize());
+            return new PageImpl(namedQuery.list(), pageable, UNKNOWN_COUNT);
+        }
+        else
+            return new PageImpl(namedQuery.list());
 	}
 
     @Override
@@ -69,12 +73,12 @@ public class WMQueryExecutorImpl implements WMQueryExecutor {
 		Session currentSession = template.getSessionFactory().getCurrentSession();
 
 		SQLQuery sqlQuery = currentSession.createSQLQuery(queryString);
-        setResultTransformer(sqlQuery);
-		configureParameters(sqlQuery, params);
+        QueryHelper.setResultTransformer(sqlQuery);
+		QueryHelper.configureParameters(sqlQuery, params);
 
         if(pageable!=null)
         {
-            Long count = getCount(queryString, params, true);
+            Long count = QueryHelper.getQueryResultCount(queryString, params, true, template);
             sqlQuery.setFirstResult(pageable.getOffset());
             sqlQuery.setMaxResults(pageable.getPageSize());
             return new PageImpl(sqlQuery.list(), pageable, count);
@@ -87,12 +91,12 @@ public class WMQueryExecutorImpl implements WMQueryExecutor {
 		Session currentSession = template.getSessionFactory().getCurrentSession();
 
 		Query hqlQuery = currentSession.createQuery(queryString);
-        setResultTransformer(hqlQuery);
-		configureParameters(hqlQuery, params);
+        QueryHelper.setResultTransformer(hqlQuery);
+		QueryHelper.configureParameters(hqlQuery, params);
 
         if(pageable!=null)
         {
-            Long count = getCount(queryString, params, false);
+            Long count = QueryHelper.getQueryResultCount(queryString, params, false, template);
             hqlQuery.setFirstResult(pageable.getOffset());
             hqlQuery.setMaxResults(pageable.getPageSize());
             return new PageImpl(hqlQuery.list(), pageable, count);
@@ -100,44 +104,4 @@ public class WMQueryExecutorImpl implements WMQueryExecutor {
         else
             return new PageImpl(hqlQuery.list());
 	}
-
-    protected void configureParameters(Query query, Map<String, Object> params) {
-        String[] namedParameters = query.getNamedParameters();
-        if(namedParameters != null && namedParameters.length > 0) {
-            if(params == null || params.isEmpty())
-                throw new RuntimeException("Require input parameters such as: " + Arrays.asList(namedParameters));
-
-            for (String namedParameter : namedParameters) {
-                Object val = params.get(namedParameter);
-                if(val == null)
-                    throw new RuntimeException("No value provided for parameter name: " + namedParameter);
-                query.setParameter(namedParameter, val);
-            }
-        }
-    }
-
-    private Long getCount(String queryStr, Map<String, Object> params, boolean isNative)
-    {
-        String strQuery = QueryHelper.getCountQuery(queryStr, params);
-        Query query=null;
-        if(isNative)
-            query = template.getSessionFactory().getCurrentSession().createSQLQuery(strQuery);
-        else
-            query=template.getSessionFactory().getCurrentSession().createQuery(strQuery);
-        Long count = ((Number) query.uniqueResult()).longValue();
-        return count;
-    }
-
-    private void setResultTransformer(Query namedQuery) {
-        String[] returnAliases = namedQuery.getReturnAliases();
-        if(returnAliases != null) {
-            LOGGER.debug("return aliases : {}" , Arrays.asList(returnAliases));
-        } else {
-            LOGGER.debug("return aliases is null" );
-        }
-
-        if(returnAliases != null)
-            namedQuery.setResultTransformer(Transformers.ALIAS_TO_ENTITY_MAP);
-    }
-
 }
--- wavemaker-runtime/src/main/java/com/wavemaker/runtime/data/dao/util/QueryHelper.java
+++ wavemaker-runtime/src/main/java/com/wavemaker/runtime/data/dao/util/QueryHelper.java
@@ -1,26 +1,88 @@
 package com.wavemaker.runtime.data.dao.util;
 
-import java.util.List;
+import java.util.Arrays;
 import java.util.Map;
 
 import org.apache.commons.lang.StringUtils;
+import org.hibernate.Query;
+import org.hibernate.SQLQuery;
+import org.hibernate.transform.Transformers;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import com.wavemaker.runtime.data.exception.QueryParameterMismatchException;
-import com.wavemaker.runtime.data.model.CustomQueryParam;
+import org.springframework.orm.hibernate4.HibernateTemplate;
 
 public class QueryHelper {
 
-    private static final String SELECT_COUNT = "select count(*) ";
+    private static final String SELECT_COUNT = "select count(*) from (";
+    private static final String ALIAS=" ) as tempTable";
     private static final String FROM = " FROM ";
     private static final String FROM_HQL = "FROM ";//For a Select (*) hibernate query.
+    private static final String SELECT_COUNT1 = "select count(*) ";
+    private static final String GROUP_BY = " group by ";
 
     private static final Logger LOGGER = LoggerFactory
             .getLogger(QueryHelper.class);
 
-    public static String getCountQuery(String query, Map<String, Object> params)
-            throws QueryParameterMismatchException {
+    public static void configureParameters(Query query, Map<String, Object> params) {
+        String[] namedParameters = query.getNamedParameters();
+        if(namedParameters != null && namedParameters.length > 0) {
+            if(params == null || params.isEmpty())
+                throw new RuntimeException("Require input parameters such as: " + Arrays.asList(namedParameters));
+
+            for (String namedParameter : namedParameters) {
+                Object val = params.get(namedParameter);
+                if(val == null)
+                    throw new RuntimeException("No value provided for parameter name: " + namedParameter);
+                query.setParameter(namedParameter, val);
+            }
+        }
+    }
+
+
+    public static void setResultTransformer(Query query) {
+        if(query instanceof SQLQuery)
+        {
+            query.setResultTransformer(Transformers.ALIAS_TO_ENTITY_MAP);
+            return;
+        }
+        else
+        {
+            String[] returnAliases = query.getReturnAliases();
+            if(returnAliases != null) {
+               LOGGER.debug("return aliases : {}" , Arrays.asList(returnAliases));
+             } else {
+               LOGGER.debug("return aliases is null" );
+           }
+        if(returnAliases != null)
+            query.setResultTransformer(Transformers.ALIAS_TO_ENTITY_MAP);
+        }
+    }
+
+
+    public static Long getQueryResultCount(String queryStr, Map<String, Object> params, boolean isNative, HibernateTemplate template)
+    {
+
+        return getCountFromCountStringQuery(queryStr, params, isNative, template);
+
+    }
+
+    private static Long getCountFromCountStringQuery(String queryStr, Map<String, Object> params, boolean isNative, HibernateTemplate template) {
+        try{
+        String strQuery = getCountQuery(queryStr, params, isNative);
+        Query query=null;
+        if(isNative)
+            query = template.getSessionFactory().getCurrentSession().createSQLQuery(strQuery);
+        else
+            query=template.getSessionFactory().getCurrentSession().createQuery(strQuery);
+        return ((Number) query.uniqueResult()).longValue();
+        }
+        catch(Exception ex)
+        {
+            LOGGER.error("Count query operation failed", ex);
+            return -1L;
+        }
+    }
+    private static String getCountQuery(String query, Map<String, Object> params, boolean isNative) {
         LOGGER.debug("Getting count query for query {} with params {}", query, params);
         if (params != null) {
             for (String key : params.keySet()) {
@@ -29,9 +91,7 @@ public class QueryHelper {
                 String parameterPlaceholder = new StringBuilder(":").append(
                         queryParamName).toString();
                 if (!query.contains(parameterPlaceholder)) {
-                    throw new QueryParameterMismatchException("Parameter "
-                            + queryParamName
-                            + " does not exist in named query.");
+                    continue;
                 } else {
                     query = StringUtils.replace(query, parameterPlaceholder,
                             String.valueOf(queryParamValue));
@@ -39,18 +99,28 @@ public class QueryHelper {
             }
         }
         LOGGER.debug("Got query string after placing params {}", query);
-        query=query.trim();
-        int index = StringUtils.indexOfIgnoreCase(query,FROM_HQL );
-        if(index!=0)
-        {
-            index=StringUtils.indexOfIgnoreCase(query, FROM);
+        query = query.trim();
+        if (isNative) {
+            String countQuery = SELECT_COUNT + query + ALIAS;
+            LOGGER.debug("Got count query string {}", countQuery);
+            return countQuery;
 
+        } else {
+            int index = StringUtils.indexOfIgnoreCase(query, GROUP_BY);
+            if(index>=0)
+            {
+                return null;
+            }
+            index = StringUtils.indexOfIgnoreCase(query, FROM_HQL);
+            if (index != 0) {
+                index = StringUtils.indexOfIgnoreCase(query, FROM);
+
+            }
+            if (index < 0)
+                throw new RuntimeException("Malformed query : " + query);
+            String subQuery = query.substring(index, query.length());
+            return SELECT_COUNT1+subQuery;
         }
-        if(index<0)
-            throw new RuntimeException("Malformed query : "+query);
-        String subQuery = query.substring(index, query.length());
-        String countQuery=SELECT_COUNT + subQuery;
-        LOGGER.debug("Got count query string {}", countQuery);
-        return countQuery;
+
     }
 }
