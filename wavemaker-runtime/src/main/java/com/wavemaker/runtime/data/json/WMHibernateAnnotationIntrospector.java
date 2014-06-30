5b843c87f74b7d3a3d765c273f5f8727163cd1cc
GKS <gaurav.sharma@imaginea.com>
2014-06-30 18:11:25 +0530

    WIP Fix for the issues: WMS-570: Unable to insert data into tables when One-one relation is present between the tables
    Change-Id: I4d08a387f2c65fc8ccc2c823c44962bc9d63ca39

--- wavemaker-runtime/src/main/java/com/wavemaker/runtime/data/json/WMHibernateAnnotationIntrospector.java
+++ wavemaker-runtime/src/main/java/com/wavemaker/runtime/data/json/WMHibernateAnnotationIntrospector.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.introspect.AnnotatedMember;
 import com.fasterxml.jackson.datatype.hibernate4.HibernateAnnotationIntrospector;
 
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 
 /**
  * Created by venuj on 16-06-2014.
@@ -14,7 +15,7 @@ public class WMHibernateAnnotationIntrospector extends HibernateAnnotationIntros
     public boolean hasIgnoreMarker(AnnotatedMember m) {
         boolean ignored = super.hasIgnoreMarker(m);
         if(!ignored) {
-            ignored = m.hasAnnotation(OneToMany.class);
+            ignored = (m.hasAnnotation(OneToMany.class)||m.hasAnnotation(OneToOne.class));
         }
         return ignored;
     }
