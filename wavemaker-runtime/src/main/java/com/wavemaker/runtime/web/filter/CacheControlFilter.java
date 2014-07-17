1bd0fb951f2cba46b28a6d41be8c822b4a302987
Uday Shankar <uday.adepu@wavemaker.com>
2014-07-17 16:13:45 +0530

    Fixed Issue WMS-3(Caching Issue:- Main page is displaying previously saved content at run result.) Added no cache filter for paths '/','/index.html','login.html'
    Change-Id: I34fcd3b7b8f22ef730cddc2bd6ed7be01bfb0918

---
+++ wavemaker-runtime/src/main/java/com/wavemaker/runtime/web/filter/CacheControlFilter.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (C) 2014 WaveMaker, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.wavemaker.runtime.web.filter;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.GenericFilterBean;
+
+/**
+ * @author Uday Shankar
+ */
+@Component("cacheControlFilter")
+public class CacheControlFilter extends GenericFilterBean {
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        // set cache directives
+        HttpServletResponse httpServletResponse = (HttpServletResponse) servletResponse;
+        httpServletResponse.setHeader("Cache-Control", "no-cache, no-store, must-revalidate"); // HTTP 1.1
+        httpServletResponse.setHeader("Pragma", "no-cache"); // HTTP 1.0
+        httpServletResponse.setDateHeader("Expires", 0); // Proxies.
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+}
