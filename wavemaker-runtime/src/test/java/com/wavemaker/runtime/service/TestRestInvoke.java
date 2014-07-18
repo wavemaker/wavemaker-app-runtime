3a3eb1c1a9685a8825180426ec4614f8bfff961b
vibhus <vibhu.singhal@wavemaker.com>
2014-07-18 12:44:18 +0530

    RestService: fix for xml response parsing for a REST service
    Change-Id: I548f6bf7031447f93d060e0200e57edb6ae5833e

--- wavemaker-runtime/src/test/java/com/wavemaker/runtime/service/TestRestInvoke.java
+++ wavemaker-runtime/src/test/java/com/wavemaker/runtime/service/TestRestInvoke.java
@@ -1,9 +1,12 @@
 package com.wavemaker.runtime.service;
 
+import com.wavemaker.common.util.Tuple;
+import com.wavemaker.runtime.helper.SchemaConversionHelper;
 import com.wavemaker.runtime.rest.model.RestRequestInfo;
 import com.wavemaker.runtime.rest.model.RestResponse;
 import com.wavemaker.runtime.rest.service.RestConnector;
 import com.wavemaker.runtime.rest.service.RestRuntimeService;
+import net.sf.json.JSON;
 import org.springframework.http.MediaType;
 
 import java.io.IOException;
@@ -17,20 +20,30 @@ public class TestRestInvoke {
 
 
         RestRequestInfo restRequestInfo = new RestRequestInfo();
-        restRequestInfo.setBasicAuth(true);
-        restRequestInfo.setUserName("manager");
-        restRequestInfo.setPassword("manager");
-        restRequestInfo.setEndpointAddress("http://localhost:8080/manager/html/list");
+        restRequestInfo.setBasicAuth(false);
+        //restRequestInfo.setUserName("manager");
+        //restRequestInfo.setPassword("manager");
+        restRequestInfo.setEndpointAddress("http://maps.googleapis.com/maps/api/directions/json?origin=Toronto&destination=Montreal&sensor=false");
         restRequestInfo.setMethod("GET");
 
         System.out.println(restRequestInfo);
 
         RestResponse restResponse = new RestConnector().invokeRestCall(restRequestInfo);
+
+        String responseBody = restResponse.getResponseBody();
+        if(restResponse.getContentType() != null) {
+            MediaType responseContentType = MediaType.parseMediaType(restResponse.getContentType());
+            if (MediaType.APPLICATION_XML.getSubtype().equals(responseContentType.getSubtype())) {
+                Tuple.Two<String, JSON> rootKeyVsJsonObject = SchemaConversionHelper.convertXmlToJson(responseBody);
+                restResponse.setConvertedResponse(rootKeyVsJsonObject.v2.toString());
+            }
+        }
+
         MediaType responseContentType = MediaType.parseMediaType(restResponse.getContentType());
         System.out.println("responseContenType: " + responseContentType);
 
-        String responseBody = restResponse.getResponseBody();
-        System.out.println("responseBody: " + responseBody);
+        String responseBodyTxt = restResponse.getResponseBody();
+        System.out.println("responseBody: " + responseBodyTxt);
     }
 
 }
