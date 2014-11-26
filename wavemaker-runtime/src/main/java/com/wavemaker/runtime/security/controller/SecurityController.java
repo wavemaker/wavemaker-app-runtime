0c4e8319fe705b387f3793dd3850481636e1e362
sunilp <sunil.pulugula@wavemaker.com>
2014-11-26 16:35:21 +0530

    Providing access for security services even though security is not enabled in app,with migration script.
    Change-Id: Ie747f0315385ef5878a776c04b2405207fc6d575

--- wavemaker-runtime/src/main/java/com/wavemaker/runtime/security/controller/SecurityController.java
+++ wavemaker-runtime/src/main/java/com/wavemaker/runtime/security/controller/SecurityController.java
@@ -4,7 +4,6 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,7 +19,6 @@ import com.wavemaker.runtime.security.WMCurrentUser;
  * Created by nileshk on 5/11/14.
  */
 @RestController
-
 @RequestMapping(value = "/security")
 public class SecurityController {
 
