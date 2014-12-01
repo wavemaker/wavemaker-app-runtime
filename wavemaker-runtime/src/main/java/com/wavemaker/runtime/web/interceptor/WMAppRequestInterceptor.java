ff4ab07c4e3b01272b3e496147eadfd22c0be1c0
Sowmya Dumpa <sowmya.dumpa@wavemaker.com>
2014-12-01 19:44:56 +0530

    Interceptor for project along with migration
    Change-Id: I40020749a7d75187ab2aef52f90be2acc660fc41

---
+++ wavemaker-runtime/src/main/java/com/wavemaker/runtime/web/interceptor/WMAppRequestInterceptor.java
@@ -0,0 +1,44 @@
+package com.wavemaker.runtime.web.interceptor;
+
+import java.lang.annotation.Annotation;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.wavemaker.common.WMRuntimeException;
+import com.wavemaker.tools.api.core.annotations.WMAccessVisibility;
+import com.wavemaker.tools.api.core.models.AccessSpecifier;
+
+/**
+ * @Author: sowmyad
+ */
+public class WMAppRequestInterceptor implements HandlerInterceptor{
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        if (handler instanceof HandlerMethod) {
+            HandlerMethod handlerMethod = (HandlerMethod) handler;
+            if (handlerMethod != null && handlerMethod.getMethod().getAnnotations() != null) {
+                for (Annotation annotation : handlerMethod.getMethod().getAnnotations()) {
+                    if(annotation instanceof WMAccessVisibility && ((WMAccessVisibility)annotation).value().equals(AccessSpecifier.UNAVAILABLE)){
+                        throw new WMRuntimeException("Access denied for this method " + handlerMethod.getMethod().getName());
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+    }
+
+}
