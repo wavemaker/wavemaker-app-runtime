ad3fade7f18890e6ce818bb458a6218e28d02256
Uday Shankar <uday.adepu@wavemaker.com>
2014-12-18 20:46:50 +0530

    Fixed caching issue in custom security and added documentation for the customAuthManager class
    Change-Id: Ie3c525a64ffd1cbf41171a4914250bdaea4891e6

--- wavemaker-runtime/src/main/java/com/wavemaker/runtime/security/WMCustomAuthenticationManager.java
+++ wavemaker-runtime/src/main/java/com/wavemaker/runtime/security/WMCustomAuthenticationManager.java
@@ -3,9 +3,16 @@ package com.wavemaker.runtime.security;
 import javax.servlet.http.HttpServletRequest;
 
 /**
+ * The class which needs to be implemented by users who would like to have their custom authentication mechanism
  * @author Uday Shankar
  */
 public interface WMCustomAuthenticationManager {
 
+    /**
+     * should return WMUser object if the authentication details can be extracted from httpServletRequest through cookie or some other headers.
+     * Otherwise the method should return null
+     * @param httpServletRequest
+     * @return
+     */
     public WMUser authenticate(HttpServletRequest httpServletRequest);
 }
