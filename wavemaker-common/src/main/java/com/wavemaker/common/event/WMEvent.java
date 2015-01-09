fd8244b80d11b4a5688f52ce29097813eb2dfbc7
Uday Shankar <uday.adepu@wavemaker.com>
2015-01-09 12:36:05 +0530

    Added causedBy to event and add event ProjectServiceDeleted and deployment manager listening on ProjectUpgradeEvents
    Change-Id: Ibc56f88d506db29a27fa65532c2d9a34abdb1cfc

--- wavemaker-common/src/main/java/com/wavemaker/common/event/WMEvent.java
+++ wavemaker-common/src/main/java/com/wavemaker/common/event/WMEvent.java
@@ -7,7 +7,18 @@ import org.springframework.context.ApplicationEvent;
  */
 public abstract class WMEvent extends ApplicationEvent {
 
+    private Object causedBy;
+
     public WMEvent(String source) {
         super(source);
+    }
+
+    public WMEvent(String source, Object causedBy) {
+        super(source);
+        this.causedBy = causedBy;
+    }
+
+    public Object getCausedBy() {
+        return causedBy;
     }
 }
