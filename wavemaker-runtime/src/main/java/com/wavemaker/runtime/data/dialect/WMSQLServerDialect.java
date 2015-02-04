15414ab3401ed45f9efa0c50a1118423b01d2d05
sunilp <sunil.pulugula@wavemaker.com>
2015-02-04 18:47:36 +0530

    Fixed two blockers (WMS-2927 & WMS-2928) : Error Thrown as 'Error in fetching query results. : MappingException: No Dialect mapping for JDBC type: -9' on Executing Native SQL for Imported MS SQL DB Error Thrown as 'Error Error in executing procedure. : SQLServerException: Incorrect syntax near '@P0'.' on trying to Execute a Stored Procedure from an Imported MS SQL DB
    Change-Id: Ib973a5e7f8c9c3146eb7ab8e0c12382dd80e3bfc

---
+++ wavemaker-runtime/src/main/java/com/wavemaker/runtime/data/dialect/WMSQLServerDialect.java
@@ -0,0 +1,19 @@
+package com.wavemaker.runtime.data.dialect;
+
+import java.sql.Types;
+
+import org.hibernate.dialect.SQLServer2012Dialect;
+import org.hibernate.type.StandardBasicTypes;
+
+/**
+ * Created by sunilp on 4/2/15.
+ */
+public class WMSQLServerDialect extends SQLServer2012Dialect {
+
+    public WMSQLServerDialect() {
+        super();
+        registerHibernateType(Types.NCHAR, StandardBasicTypes.CHARACTER.getName());
+        registerHibernateType(Types.LONGNVARCHAR, StandardBasicTypes.TEXT.getName());
+        registerHibernateType(Types.NVARCHAR, StandardBasicTypes.STRING.getName());
+    }
+}
