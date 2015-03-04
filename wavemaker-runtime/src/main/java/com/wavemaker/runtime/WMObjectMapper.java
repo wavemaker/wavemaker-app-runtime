5a21200532fed13272a794b4c8ec42fd28b9ba45
Uday Shankar <uday.adepu@wavemaker.com>
2015-03-04 15:52:14 +0530

    Revert "Using fields for serialization and deserialization instead of getters and setters for ObjectMapper"
    This reverts commit 9fef25668b89aeeccb04ebb29a8b8794d8a9622f.
    
    Change-Id: I4bba16d3c2ba02d0b0578b19b2190ba6ac9f8f7c

--- wavemaker-runtime/src/main/java/com/wavemaker/runtime/WMObjectMapper.java
+++ wavemaker-runtime/src/main/java/com/wavemaker/runtime/WMObjectMapper.java
@@ -26,7 +26,6 @@ import java.net.URL;
 
 import org.apache.commons.io.IOUtils;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.core.JsonGenerationException;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonParseException;
@@ -39,6 +38,9 @@ import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.cfg.MapperConfig;
+import com.fasterxml.jackson.databind.introspect.AnnotatedMethod;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.datatype.hibernate4.Hibernate4Module;
 import com.wavemaker.runtime.data.json.WMHibernate4Module;
@@ -46,6 +48,7 @@ import com.wavemaker.runtime.data.json.WMHibernate4Module;
 public class WMObjectMapper extends ObjectMapper {
 
     private static WMObjectMapper instance = new WMObjectMapper();
+    private static WMPropertyNamingStrategy PROPERTY_NAMING_STRATEGY = new WMPropertyNamingStrategy();
 
     private WMObjectReadMapper readMapper = null;
     private WMObjectwritMapper writeMapper = null;
@@ -225,11 +228,7 @@ public class WMObjectMapper extends ObjectMapper {
             ByteArraySerializeModule byteArrayDeserializerModule = new ByteArraySerializeModule();
             module.addSerializer(byte[].class, byteArrayDeserializerModule);
             registerModule(module);
-            setVisibilityChecker(getSerializationConfig().getDefaultVisibilityChecker()
-                    .withFieldVisibility(JsonAutoDetect.Visibility.ANY)
-                    .withGetterVisibility(JsonAutoDetect.Visibility.NONE)
-                    .withSetterVisibility(JsonAutoDetect.Visibility.NONE)
-                    .withCreatorVisibility(JsonAutoDetect.Visibility.NONE));
+            setPropertyNamingStrategy(PROPERTY_NAMING_STRATEGY);
         }
     }
 
@@ -245,11 +244,46 @@ public class WMObjectMapper extends ObjectMapper {
             ByteArraySerializeModule byteArrayDeserializerModule =  new ByteArraySerializeModule();
             module.addSerializer(byte[].class, byteArrayDeserializerModule);
             registerModule(module);
-            setVisibilityChecker(getSerializationConfig().getDefaultVisibilityChecker()
-                    .withFieldVisibility(JsonAutoDetect.Visibility.ANY)
-                    .withGetterVisibility(JsonAutoDetect.Visibility.NONE)
-                    .withSetterVisibility(JsonAutoDetect.Visibility.NONE)
-                    .withCreatorVisibility(JsonAutoDetect.Visibility.NONE));
+            setPropertyNamingStrategy(PROPERTY_NAMING_STRATEGY);
         }
     }
+
+    private static class WMPropertyNamingStrategy extends PropertyNamingStrategy {
+
+        private static final String[] POSSIBLE_GET_METHOD_START_NAMES = new String[] {"get", "is"};
+        private static final String[] POSSIBLE_SET_METHOD_START_NAMES = new String[] {"set"};
+
+        @Override
+        public String nameForGetterMethod(MapperConfig<?> config, AnnotatedMethod method, String defaultName) {
+            return getPossibleFieldName(method, defaultName, POSSIBLE_GET_METHOD_START_NAMES);
+        }
+
+        @Override
+        public String nameForSetterMethod(MapperConfig<?> config, AnnotatedMethod method, String defaultName) {
+            return getPossibleFieldName(method, defaultName, POSSIBLE_SET_METHOD_START_NAMES);
+        }
+
+        private String getPossibleFieldName(AnnotatedMethod method, String defaultName, String[] possibleMethodStartNames) {
+            String name = method.getName();
+            for (int i=0; i< possibleMethodStartNames.length; i++) {
+                String possibleStartName = possibleMethodStartNames[i];
+                if (name.startsWith(possibleStartName)) {
+                    String remPart = name.substring(possibleStartName.length());
+                    if (remPart.length() == 0) {
+                        break;
+                    }
+                    char upper = remPart.charAt(0);
+                    char lower = Character.toLowerCase(upper);
+                    if (lower == upper) {
+                        break;
+                    }
+                    StringBuilder sb = new StringBuilder(remPart);
+                    sb.setCharAt(0, lower);
+                    return sb.toString();
+                }
+            }
+            return defaultName;
+        }
+    }
+
 }
