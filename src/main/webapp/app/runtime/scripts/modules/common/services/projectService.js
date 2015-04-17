906e7c9f4e613d2778b9e25f5ce1b1e69ef118a7
Kiran Parvathaneni <kiran.parvathaneni@wavemaker.com>
2015-04-17 16:11:48 +0530

    Fix for run mode getId error and moved deployedUrl to run
    Change-Id: I3f98ec9855ac8e62de41d94dd197046d3b082722

--- src/main/webapp/app/runtime/scripts/modules/common/services/projectService.js
+++ src/main/webapp/app/runtime/scripts/modules/common/services/projectService.js
@@ -7,7 +7,7 @@
  * The `ProjectService` provides the details about the project apis.
  */
 
-wm.modules.wmCommon.services.ProjectService = function (BaseService, CONSTANTS) {
+wm.modules.wmCommon.services.ProjectService = function (BaseService, CONSTANTS, $location) {
     'use strict';
 
     function run(details, successCallback, failureCallback) {
@@ -21,6 +21,48 @@ wm.modules.wmCommon.services.ProjectService = function (BaseService, CONSTANTS) 
             },
             config: details.config
         }, successCallback, failureCallback);
+    }
+
+    function getDeployedUrl() {
+        var locationUrl,
+            lastIndex,
+            projectDeployedUrl;
+
+        /*If $location.$$absUrl is of the form http://localhost:8080/WM2c908a52446c435b01446cfdacf50013/#/Main,
+         * remove # and the page name to get only the deployed url of the project.*/
+        if (CONSTANTS.isRunMode) {
+            locationUrl = $location.$$absUrl;
+            lastIndex = locationUrl.split("#")[0].lastIndexOf("/");
+            projectDeployedUrl = locationUrl.substr(0, lastIndex);
+            lastIndex = projectDeployedUrl.lastIndexOf("/");
+            if (projectDeployedUrl[lastIndex + 1] === "#") {
+                projectDeployedUrl = projectDeployedUrl.substr(0, lastIndex);
+            }
+            if (projectDeployedUrl.indexOf('http') !== -1) {
+                /*Removing the protocol from the url*/
+                if (projectDeployedUrl.indexOf('https:') !== -1) {
+                    projectDeployedUrl = projectDeployedUrl.substr(6);
+                } else {
+                    projectDeployedUrl = projectDeployedUrl.substr(5);
+                }
+            }
+
+            return projectDeployedUrl;
+        }
+    }
+
+    /**
+     * @ngdoc function
+     * @name wm.common.$ProjectService#getId
+     * @methodOf wm.common.$ProjectService
+     * @function
+     *
+     * @description
+     * returns the id of the project service based on the 'mode'.
+     *
+     */
+    function getId() {
+        return $location.$$absUrl.split('/')[3];
     }
 
     /* APIs returned by the ProjectService.*/
@@ -39,7 +81,23 @@ wm.modules.wmCommon.services.ProjectService = function (BaseService, CONSTANTS) 
          * @param {function} successCallback to called on success
          * @param {function} failureCallback to called on failure
          */
-        run: run
+        run: run,
+        /**
+         * @ngdoc function
+         * @name wm.common.$ProjectService#getDeployedUrl
+         * @methodOf wm.common.$ProjectService
+         * @function
+         *
+         * @description
+         * Returns deployed url
+         *
+         */
+        getDeployedUrl : getDeployedUrl,
+        /*
+        * Returns project Id
+        *
+        * */
+        getId : getId
     };
 
 };
