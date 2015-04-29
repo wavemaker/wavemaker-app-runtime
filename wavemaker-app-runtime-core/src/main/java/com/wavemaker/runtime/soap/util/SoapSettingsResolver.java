1ac1c3875c542053044c7f721481006ef55be53e
Venugopal Jidigam <venugopal.jidigam@wavemaker.com>
2015-04-29 09:59:44 +0530

    support for soap service settings
    Change-Id: Ie3bfc333782dbf89a09ad3f10a0fd33c631bce85

---
+++ wavemaker-app-runtime-core/src/main/java/com/wavemaker/runtime/soap/util/SoapSettingsResolver.java
@@ -0,0 +1,74 @@
+/**
+ * Copyright (C) 2014 WaveMaker, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.wavemaker.runtime.soap.util;
+
+import javax.xml.ws.BindingProvider;
+import javax.xml.ws.handler.MessageContext;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import com.sun.xml.ws.developer.JAXWSProperties;
+import com.wavemaker.runtime.soap.SoapServiceSettings;
+import org.apache.commons.lang.StringUtils;
+
+/**
+ * @author Frankie Fu
+ */
+public class SoapSettingsResolver {
+
+    @SuppressWarnings("unchecked")
+    public static void setBindingProperties(BindingProvider service, SoapServiceSettings bindingProperties) {
+        if (bindingProperties != null) {
+            Map<String, Object> requestContext = service.getRequestContext();
+
+            if (StringUtils.isNotEmpty(bindingProperties.getEndpointAddress())) {
+                requestContext.put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, bindingProperties.getEndpointAddress());
+            }
+
+            if (StringUtils.isNotEmpty(bindingProperties.getHttpBasicAuthUsername())) {
+                requestContext.put(BindingProvider.USERNAME_PROPERTY, bindingProperties.getHttpBasicAuthUsername());
+                requestContext.put(BindingProvider.PASSWORD_PROPERTY, bindingProperties.getHttpBasicAuthPassword());
+            }
+
+            requestContext.put(JAXWSProperties.CONNECT_TIMEOUT, Integer.valueOf(bindingProperties.getConnectionTimeout()));
+            requestContext.put(JAXWSProperties.REQUEST_TIMEOUT, Integer.valueOf(bindingProperties.getRequestTimeout()));
+
+            if (StringUtils.isNotEmpty(bindingProperties.getSoapActionURI())) {
+                requestContext.put(BindingProvider.SOAPACTION_USE_PROPERTY, true);
+                requestContext.put(BindingProvider.SOAPACTION_URI_PROPERTY, bindingProperties.getSoapActionURI());
+            }
+
+            Map<String, String> httpHeaders = bindingProperties.getHttpHeaders();
+            if (httpHeaders != null && !httpHeaders.isEmpty()) {
+                Map<String, List<String>> reqHeaders = (Map<String, List<String>>) requestContext.get(MessageContext.HTTP_REQUEST_HEADERS);
+                if (reqHeaders == null) {
+                    reqHeaders = new HashMap<String, List<String>>();
+                    requestContext.put(MessageContext.HTTP_REQUEST_HEADERS, reqHeaders);
+                }
+                for (Entry<String, String> entry : httpHeaders.entrySet()) {
+                    List<String> list = new ArrayList<String>();
+                    list.add(entry.getValue());
+
+                    reqHeaders.put(entry.getKey(), list);
+                }
+            }
+        }
+    }
+
+}
