69725d14fe35b2e56e7e1b928d0675ee70584213
abhilashsandi <abhilash.sandi@wavemaker.com>
2015-06-26 19:58:17 +0530

    Fix for issue: Slider not working in livefrom
    Change-Id: I516f0fd959c04e9c7a48a018840d00331282af47

--- src/main/webapp/scripts/modules/widgets/live/form/liveform.js
+++ src/main/webapp/scripts/modules/widgets/live/form/liveform.js
@@ -1044,9 +1044,14 @@ WM.module('wm.widgets.live')
                         '<wm-label class="col-md-3 col-sm-3" caption="{{dataArray[' + index + '].displayName}}" hint="{{dataArray[' + index + '].displayName}}" required="{{dataArray[' + index + '].required}}"></wm-label>' +
                         '<div class="col-md-9 col-sm-9">' +
                             '<wm-label class="form-control-static" caption="{{dataArray[' + index + '].value}}" show="{{!isUpdateMode}}"></wm-label>' +
-                            '<wm-slider name="{{dataArray[' + index + '].key}}" required="{{dataArray[' + index + '].required}}" readonly="{{dataArray[' + index + '].readonly}}" show="{{isUpdateMode}}" scopedatavalue="dataArray[' + index + '].value" minvalue="{{dataArray[' + index + '].minvalue}}" maxvalue="{{dataArray[' + index + '].maxvalue}}" step="' + step + '"></wm-slider>' +
-                        '</div>' +
-                    '</wm-composite>';
+                            '<wm-slider name="{{dataArray[' + index + '].key}}" required="{{dataArray[' + index + '].required}}" readonly="{{dataArray[' + index + '].readonly}}" show="{{isUpdateMode}}" scopedatavalue="dataArray[' + index + '].value" step="' + step + '"';
+                if (fieldDef.minvalue) {
+                    template = template + ' minvalue="{{dataArray[' + index + '].minvalue}}"';
+                }
+                if (fieldDef.maxvalue) {
+                    template = template + ' maxvalue="{{dataArray[' + index + '].maxvalue}}"';
+                }
+                template = template + '></wm-slider></div></wm-composite>';
                 break;
             case "Upload":
                 template = template +
