61e9d364895d6b096821d1e2bd8c88e4cc5d374a
abhilashsandi <abhilash.sandi@wavemaker.com>
2015-07-14 12:27:09 +0530

    Fix for WMS-4157: Time is shown as date in live form when row is selected in in grid
    Change-Id: I41c588577668aecee5d4f03f7697fa5c7409cd62

--- src/main/webapp/scripts/modules/widgets/live/form/liveform.js
+++ src/main/webapp/scripts/modules/widgets/live/form/liveform.js
@@ -999,7 +999,7 @@ WM.module('wm.widgets.live')
                     '<wm-composite widget="date" show="{{dataArray[' + index + '].show}}" class="form-time {{dataArray[' + index + '].class}}">' +
                         '<wm-label class="col-md-3 col-sm-3" caption="{{dataArray[' + index + '].displayName}}" hint="{{dataArray[' + index + '].displayName}}" required="{{dataArray[' + index + '].required}}"></wm-label>' +
                         '<div class="col-md-9 col-sm-9">' +
-                            '<wm-label class="form-control-static" caption="{{dataArray[' + index + '].value | date:\'dd-MMM-yyyy\'}}" show="{{!isUpdateMode}}"></wm-label>' +
+                            '<wm-label class="form-control-static" caption="{{dataArray[' + index + '].value | date:\'HH:mm:ss\'}}" show="{{!isUpdateMode}}"></wm-label>' +
                             '<wm-time name="{{dataArray[' + index + '].key}}" required="{{dataArray[' + index + '].required}}" ' +
                                 'regexp="{{dataArray[' + index + '].regexp}}" scopedatavalue="dataArray[' + index + '].value" show="{{isUpdateMode}}" outputformat="{{dataArray[' + index + '].outputformat}}">' +
                             '</wm-time>' +
@@ -1012,7 +1012,7 @@ WM.module('wm.widgets.live')
                     '<wm-composite widget="date" show="{{dataArray[' + index + '].show}}" class="form-timestamp {{dataArray[' + index + '].class}}">' +
                         '<wm-label class="col-md-3 col-sm-3" caption="{{dataArray[' + index + '].displayName}}" hint="{{dataArray[' + index + '].displayName}}" required="{{dataArray[' + index + '].required}}"></wm-label>' +
                         '<div class="col-md-3 col-sm-4">' +
-                            '<wm-label class="form-control-static" caption="{{dataArray[' + index + '].value | date:\'dd-MMM-yyyy hh:mm:ss\'}}" show="{{!isUpdateMode}}"></wm-label>' +
+                            '<wm-label class="form-control-static" caption="{{dataArray[' + index + '].value | date:\'dd-MMM-yyyy HH:mm:ss\'}}" show="{{!isUpdateMode}}"></wm-label>' +
                             '<wm-datetime name="{{dataArray[' + index + '].key}}" required="{{dataArray[' + index + '].required}}" scopedatavalue="dataArray[' + index + '].value" show="{{isUpdateMode}}" outputformat="{{dataArray[' + index + '].outputformat}}"  mindate="{{dataArray[' + index + '].minvalue}}" maxdate="{{dataArray[' + index + '].maxvalue}}"';
                 if (fieldDef.datepattern) {
                     template = template + ' datepattern="{{dataArray[' + index + '].datepattern}}"';
