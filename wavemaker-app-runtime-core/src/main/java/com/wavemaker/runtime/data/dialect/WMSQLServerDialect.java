c67ba21be425682193cd4b8c79017dd7e2087037
sunilkumar <sunil.pulugula@wavemaker.com>
2015-07-21 20:40:07 +0530

    Fixed : Mssql export and import datetime issues.
    Change-Id: Ic3baa9da50abf9f2f4c1a48d32c52205279d66e1
    (cherry picked from commit 2b70d4a32e47eb2026d002f4f60dc8264621fb21)

--- wavemaker-app-runtime-core/src/main/java/com/wavemaker/runtime/data/dialect/WMSQLServerDialect.java
+++ wavemaker-app-runtime-core/src/main/java/com/wavemaker/runtime/data/dialect/WMSQLServerDialect.java
@@ -5,6 +5,8 @@ import java.sql.Types;
 import org.hibernate.dialect.SQLServer2012Dialect;
 import org.hibernate.type.StandardBasicTypes;
 
+import com.wavemaker.studio.common.CommonConstants;
+
 /**
  * Created by sunilp on 4/2/15.
  */
@@ -17,5 +19,6 @@ public class WMSQLServerDialect extends SQLServer2012Dialect {
         registerHibernateType(Types.NVARCHAR, StandardBasicTypes.STRING.getName());
 
         registerColumnType(Types.CHAR, "nchar(1)");
+        registerColumnType(CommonConstants.DATE_TIME_WM_TYPE_CODE, "datetime" );
     }
 }
