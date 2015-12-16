4a0b8c2aa0c780a81df8b24708e08737b4b544ec
vinay <vinay.kaparthi@wavemaker.com>
2015-12-16 16:01:44 +0530

    Widgets - richTextEditor: Implement getCursorPosition method.
    Change-Id: I6ae52dda23c21a18b3e6214dc6b508d067351a50

--- src/main/webapp/scripts/modules/widgets/form/richtexteditor/richtexteditor.js
+++ src/main/webapp/scripts/modules/widgets/form/richtexteditor/richtexteditor.js
@@ -3,104 +3,132 @@
 
 WM.module('wm.widgets.form').requires = WM.module('wm.widgets.form').requires.concat(['textAngular']);
 WM.module('wm.widgets.form')
-    .run(['$templateCache', function ($templateCache) {
+    .run(['$templateCache', function ($tc) {
         'use strict';
-        $templateCache.put('template/widget/richtexteditor.html',
-            '<div class="app-richtexteditor clearfix" init-widget has-model apply-styles data-ng-show="show" role="input">' +
-                '<div text-angular data-ng-model="_model_"></div>' +
-                '<div data-ng-bind-html="_model_" class="ta-preview" data-ng-if="showpreview"></div>' +
-                /*Holder for the model for submitting values in a form*/
-                '<input class="model-holder ng-hide" data-ng-disabled="disabled">' +
+        $tc.put('template/widget/richtexteditor.html',
+            '<div class="app-richtexteditor clearfix" init-widget has-model apply-styles ng-show="show" role="input">' +
+                '<div text-angular ng-model="_model_"></div>' +
+                '<div ng-bind-html="_model_" class="ta-preview" ng-if="showpreview"></div>' +
+                '<input class="model-holder ng-hide" ng-disabled="disabled">' +
             '</div>'
             );
     }])
-    .directive('wmRichtexteditor', ['PropertiesFactory', '$templateCache', 'WidgetUtilService', function (PropertiesFactory, $templateCache, WidgetUtilService) {
-        'use strict';
-        var widgetProps = PropertiesFactory.getPropertiesOf('wm.richtexteditor', ['wm.base', 'wm.base.editors']),
-            notifyFor = {
-                'placeholder': true,
-                'showpreview': true,
-                'readonly': true
-            };
+    .directive('wmRichtexteditor', [
+        'PropertiesFactory',
+        '$templateCache',
+        'WidgetUtilService',
+        'taSelection',
 
-        /* Define the property change handler. This function will be triggered when there is a change in the widget property */
-        function propertyChangeHandler(scope, editorElement, btnElements, key, newVal) {
-            switch (key) {
-            case 'placeholder':
-                scope._model_ = newVal;
-                break;
-            case 'showpreview':
-                scope.showpreview = newVal;
-                break;
-            case 'readonly':
-                /*check for newVal*/
-                if (newVal === true) {
-                    /*listen on keypress, prevent default action*/
-                    editorElement.on('keypress.readOnlyEvent', function (event) {
-                        event.preventDefault();
-                    });
-                    editorElement.on('click.readOnlyEvent', function (event) {
-                        event.preventDefault();
-                        /*to enable/disable picture, link, unlink & YouTube buttons*/
-                        btnElements.attr('disabled', newVal === true);
-                    });
-                } else {
-                    /*unbind readOnlyEvent keypress action*/
-                    editorElement.off('.readOnlyEvent');
+        function (PropertiesFactory, $tc, WidgetUtilService, taSelection) {
+            'use strict';
+
+            var widgetProps = PropertiesFactory.getPropertiesOf('wm.richtexteditor', ['wm.base', 'wm.base.editors']),
+                notifyFor = {
+                    'placeholder': true,
+                    'showpreview': true,
+                    'readonly': true
+                };
+
+            /* Define the property change handler. This function will be triggered when there is a change in the widget property */
+            function propertyChangeHandler($is, editorElement, btnElements, key, nv) {
+                switch (key) {
+                case 'placeholder':
+                    $is._model_ = nv;
+                    break;
+                case 'showpreview':
+                    $is.showpreview = nv;
+                    break;
+                case 'readonly':
+                    if (nv === true) {
+                        /*listen on keypress, prevent default action*/
+                        editorElement.on('keypress.readOnlyEvent', function (event) {
+                            event.preventDefault();
+                        });
+                        editorElement.on('click.readOnlyEvent', function (event) {
+                            event.preventDefault();
+                            /*to enable/disable picture, link, unlink & YouTube buttons*/
+                            btnElements.attr('disabled', nv === true);
+                        });
+                    } else {
+                        /*unbind readOnlyEvent keypress action*/
+                        editorElement.off('.readOnlyEvent');
+                    }
+                    break;
                 }
-                break;
             }
-        }
 
-        return {
-            'restrict': 'E',
-            'scope': {},
-            'replace': true,
-            'template': function (tElement, tAttrs) {
-                var template = WM.element($templateCache.get('template/widget/richtexteditor.html'));
-                /*Set name for the model-holder, to ease submitting a form*/
-                template.find('.model-holder').attr('name', tAttrs.name);
-                return template[0].outerHTML;
-            },
-            'compile': function () {
-                return {
-                    'pre': function (scope) {
-                        scope.widgetProps = widgetProps;
+            /**
+             * @ngdoc function
+             * @name wm.widgets.form.directive:wmRichtexteditor#getCursorPosition
+             *
+             * @description
+
+             */
+            function getCursorPosition() {
+                var selection,
+                    retObj;
+                try {
+                    selection  = taSelection.getSelection();
+                    retObj     = {
+                        'start': selection.start.offset,
+                        'end'  : selection.end.offset
+                    };
+                } catch (e) {
+                    retObj = {};
+                }
+                return retObj;
+            }
+
+            return {
+                'restrict': 'E',
+                'scope': {},
+                'replace': true,
+                'template': function (tElement, tAttrs) {
+                    var template = WM.element($tc.get('template/widget/richtexteditor.html'));
+                    /*Set name for the model-holder, to ease submitting a form*/
+                    template.find('.model-holder').attr('name', tAttrs.name);
+                    return template[0].outerHTML;
+                },
+                'link': {
+                    'pre': function ($is) {
+                        $is.widgetProps = widgetProps;
                     },
-                    'post': function (scope, element, attrs) {
+                    'post': function ($is, $el, attrs) {
                         /* register the property change handler */
                         WidgetUtilService.registerPropertyChangeListener(
-                            propertyChangeHandler.bind(undefined, scope, element.find('.ta-editor'), element.find('.fa-picture-o, .fa-link, .fa-unlink, .fa-youtube-play, .fa-code').parent('.btn')),
-                            scope,
+                            propertyChangeHandler.bind(undefined, $is, $el.find('.ta-editor'), $el.find('.fa-picture-o, .fa-link, .fa-unlink, .fa-youtube-play, .fa-code').parent('.btn')),
+                            $is,
                             notifyFor
                         );
 
-                        var hiddenInputEl = element.children('input'),
+                        var hiddenInputEl = $el.children('input'),
                             ngModelCtrl;
 
                         if (!attrs.widgetid && attrs.scopedatavalue) {
-                            scope.$on('$destroy', scope.$watch('_model_', function (newVal) {
+                            $is.$on('$destroy', $is.$watch('_model_', function (newVal) {
                                 hiddenInputEl.val(newVal);
                             }));
                         }
 
-                        ngModelCtrl = element.children('[text-angular]').controller('ngModel');
+                        ngModelCtrl = $el.children('[text-angular]').controller('ngModel');
                         ngModelCtrl.$viewChangeListeners.push(function () {
-                            scope._onChange(new Event('change'));
+                            $is._onChange(new Event('change'));
                         });
 
                         /*Called from form reset when users clicks on form reset*/
-                        scope.reset = function () {
+                        $is.reset = function () {
                             //TODO implement custom reset logic here
-                            scope._model_ = '';
+                            $is._model_ = '';
                         };
 
-                        WidgetUtilService.postWidgetCreate(scope, element, attrs);
+                        $is.getCursorPosition  = getCursorPosition;
+
+                        WidgetUtilService.postWidgetCreate($is, $el, attrs);
                     }
-                };
-            }
-        };
-    }]);
+                }
+            };
+        }
+    ]);
 
 
 /**
@@ -111,6 +139,14 @@ WM.module('wm.widgets.form')
  * @description
  * The `wmRichtexteditor` directive defines a rich text editor widget. <br>
  *
+ * <strong>method:</strong> <em>getCursorPosition</em> <br>
+ * Returns the position of the cursor if the cursor is inside the editor. <br>
+ * Returned object contains the start and end offsets. <br>
+ * ```js
+ * var position = widget.getCursorPosition(); // widget is the isolateScope of the richTextEditor widget.
+ * // In run mode widgets can be accessed as -- $scope.Widgets._widgetName_
+ * console.log(position); // prints {start: 4, end: 10}
+ * ```
  * @scope
  *
  * @requires PropertiesFactory
@@ -147,30 +183,30 @@ WM.module('wm.widgets.form')
  *                   Callback function which will be triggered when the widget value is changed.
  *
  * @example
- *   <example module="wmCore">
- *       <file name="index.html">
- *           <div data-ng-controller="Ctrl" class="wm-app">
- *               <wm-composite>
- *                   <wm-label caption="Placeholder:"></wm-label>
- *                   <wm-text scopedatavalue="placeholder"></wm-text>
- *               </wm-composite>
- *               <wm-composite>
- *                   <wm-label caption="Show:"></wm-label>
- *                   <wm-checkbox name="checkbox1" scopedatavalue="show" checked="checked"></wm-checkbox>
- *               </wm-composite>
- *               <wm-composite>
- *                   <wm-label caption="Show Preview:"></wm-label>
- *                   <wm-checkbox name="checkbox1" scopedatavalue="preview"></wm-checkbox>
- *               </wm-composite>
- *               <wm-richtexteditor name="example-richtexteditor" show='{{show}}' placeholder='{{placeholder}}' showpreview='{{preview}}'>
- *               </wm-richtexteditor>
- *           </div>
- *       </file>
- *       <file name="script.js">
- *          function Ctrl($scope) {
- *              $scope.show=true;
- *              $scope.placeholder = "Sample Text";
- *          }
- *       </file>
- *   </example>
+    <example module="wmCore">
+        <file name="index.html">
+            <div data-ng-controller="Ctrl" class="wm-app">
+                <wm-composite>
+                    <wm-label caption="Placeholder:"></wm-label>
+                    <wm-text scopedatavalue="placeholder"></wm-text>
+                </wm-composite>
+                <wm-composite>
+                    <wm-label caption="Show:"></wm-label>
+                    <wm-checkbox name="checkbox1" scopedatavalue="show" checked="checked"></wm-checkbox>
+                </wm-composite>
+                <wm-composite>
+                    <wm-label caption="Show Preview:"></wm-label>
+                    <wm-checkbox name="checkbox1" scopedatavalue="preview"></wm-checkbox>
+                </wm-composite>
+                <wm-richtexteditor name="example-richtexteditor" show='{{show}}' placeholder='{{placeholder}}' showpreview='{{preview}}'>
+                </wm-richtexteditor>
+            </div>
+        </file>
+        <file name="script.js">
+           function Ctrl($scope) {
+               $scope.show=true;
+               $scope.placeholder = "Sample Text";
+           }
+        </file>
+    </example>
  */
