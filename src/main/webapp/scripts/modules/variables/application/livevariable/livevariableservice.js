157ef2d108b675d7ff0842416563249c375167fe
SumanthKodumuru <sumanth.kodumuru@wavemaker.com>
2015-12-29 15:40:33 +0530

    Managing variable crud operations using services
    Change-Id: I6f002d6ccc846b1bbdf5b104e74d858ce3fbbad1

--- src/main/webapp/scripts/modules/variables/application/livevariable/livevariableservice.js
+++ src/main/webapp/scripts/modules/variables/application/livevariable/livevariableservice.js
@@ -183,8 +183,11 @@ wm.variables.services.$liveVariable = [
                 variable[propertyName] = propertyValue;
                 /*Check for sanity.
                 * writableVariable will be null in case of partial page variables.*/
-                if (writableVariable) {
+                if (writableVariable && !WM.equals(writableVariable[propertyName], propertyValue)) {
+                    /*Set the "saveVariables" to true so that when "save"/"run" buttons are clicked, the variables could be saved into the file.*/
+                    $rootScope.saveVariables = true;
                     writableVariable[propertyName] = propertyValue;
+                    Variables.updateVariable(writableVariable.name, writableVariable);
                 }
             },
         /*Function to fetch the meta data for the table.*/
@@ -381,9 +384,6 @@ wm.variables.services.$liveVariable = [
 
                     /*Set the propertiesMap property of the live-variable to correspond to the propertiesMap of the table*/
                     $rootScope.variables[variable.name].propertiesMap = variable.propertiesMap;
-
-                    /*Set the "saveVariables" to true so that when "save"/"run" buttons are clicked, the variables could be saved into the file.*/
-                    $rootScope.saveVariables = true;
 
                     Utils.triggerFn(callback, projectID, variable, options, success);
                 }, WM.noop);
