b8b1ad4b0e4d101fe95f3e755f690a10aa16366f
vinay <vinay.kaparthi@wavemaker.com>
2016-02-18 12:42:40 +0530

    Fix for issue: Not able to provide width for container widget.
    Change-Id: Idfda9b1ce634b73ce7d58a6a99a0a15de775b4ed

--- src/main/webapp/scripts/modules/widgets/base/Base.js
+++ src/main/webapp/scripts/modules/widgets/base/Base.js
@@ -2614,7 +2614,8 @@ WM.module('wm.widgets.base', [])
                 'zindex'                : 'zIndex'
             };
             SHELL_TYPE_IGNORE_LIST     = 'height overflow paddingunit paddingtop paddingright paddingbottom paddingleft';
-            CONTAINER_TYPE_IGNORE_LIST = SCROLLABLE_CONTAINER_TYPE_IGNORE_LIST = 'textalign width';
+            CONTAINER_TYPE_IGNORE_LIST = 'textalign';
+            SCROLLABLE_CONTAINER_TYPE_IGNORE_LIST = 'textalign width';
 
             _.keys(propNameCSSKeyMap)
                 .concat(SHELL_TYPE_IGNORE_LIST.split(' '))
