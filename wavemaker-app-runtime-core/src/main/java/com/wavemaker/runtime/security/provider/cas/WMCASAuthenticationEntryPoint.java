dae795613cc08565cde8e27342eedac9789ff44a
Arjun <arjun.sahasranam@wavemaker.com>
2016-05-17 16:56:39 +0530

    CAS-SUPPORT FOR DB ROLE
    Change-Id: I8b4f9c1e8c4a877bc6ac81641de0271997cd19f5

--- wavemaker-app-runtime-core/src/main/java/com/wavemaker/runtime/security/provider/cas/WMCASAuthenticationEntryPoint.java
+++ wavemaker-app-runtime-core/src/main/java/com/wavemaker/runtime/security/provider/cas/WMCASAuthenticationEntryPoint.java
@@ -1,13 +1,14 @@
 package com.wavemaker.runtime.security.provider.cas;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.apache.commons.lang3.StringUtils;
+import org.jasig.cas.client.util.CommonUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.cas.ServiceProperties;
 import org.springframework.security.cas.web.CasAuthenticationEntryPoint;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 /**
  * Created by ArjunSahasranam on 5/16/16.
@@ -20,14 +21,15 @@ public class WMCASAuthenticationEntryPoint extends CasAuthenticationEntryPoint {
 
     @Override
     protected String createServiceUrl(HttpServletRequest request, HttpServletResponse response) {
-        StringBuffer requestURL = request.getRequestURL();
-        String contextPath = request.getContextPath();
+        if (StringUtils.isBlank(serviceProperties.getService())) {
+            StringBuffer requestURL = request.getRequestURL();
+            String contextPath = request.getContextPath();
 
-        String serviceHostUrl = requestURL.substring(0, requestURL.indexOf(contextPath));
-        String serviceUrl = serviceHostUrl + contextPath + "/j_spring_cas_security_check";
-        if(StringUtils.isBlank(serviceProperties.getService())){
+            String serviceHostUrl = requestURL.substring(0, requestURL.lastIndexOf(contextPath));
+            String serviceUrl = serviceHostUrl + contextPath + "/j_spring_cas_security_check";
             serviceProperties.setService(serviceUrl);
         }
-        return super.createServiceUrl(request, response);
+        return CommonUtils.constructServiceUrl(null, response, this.serviceProperties.getService(), null,
+                this.serviceProperties.getArtifactParameter(), true);
     }
 }
