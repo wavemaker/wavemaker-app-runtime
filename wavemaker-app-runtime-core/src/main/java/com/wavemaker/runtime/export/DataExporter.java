9237870b754302a5d120701419b02417d4faa225
Dilip <dilip.gundu@wavemaker.com>
2016-05-19 19:34:42 +0530

    0# Removed Jasper dependency in ExportType to fix issue while generating swagger. 2. moved dynamic reports dependency to parent's dependency management.
    Change-Id: I41c531574e1d43d33c7b1a951b08838a9d53cd8f

--- wavemaker-app-runtime-core/src/main/java/com/wavemaker/runtime/export/DataExporter.java
+++ wavemaker-app-runtime-core/src/main/java/com/wavemaker/runtime/export/DataExporter.java
@@ -7,6 +7,7 @@ import java.io.Serializable;
 import org.hibernate.Session;
 
 import net.sf.dynamicreports.jasper.builder.JasperReportBuilder;
+import net.sf.dynamicreports.report.exception.DRException;
 
 /**
  * @author <a href="mailto:anusha.dharmasagar@wavemaker.com">Anusha Dharmasagar</a>
@@ -19,7 +20,8 @@ public class DataExporter<Entity extends Serializable> {
     private ExportType exportType;
     private ExportOptions exportOptions;
 
-    public DataExporter(Session session, Class<Entity> entityClass, ExportType exportType, ExportOptions exportOptions) {
+    public DataExporter(
+            Session session, Class<Entity> entityClass, ExportType exportType, ExportOptions exportOptions) {
         this.session = session;
         this.entityClass = entityClass;
         this.exportOptions = exportOptions;
@@ -35,7 +37,19 @@ public class DataExporter<Entity extends Serializable> {
                 .title(Templates.createTitleComponent(entityClass.getName()))
                 .highlightDetailOddRows();
         OutputStream reportOutputStream = new ByteArrayOutputStream();
-        exportType.formatReport(jasperReportBuilder, reportOutputStream);
+        try {
+            switch (exportType) {
+                case EXCEL:
+                    jasperReportBuilder.toExcelApiXls(reportOutputStream);
+                    break;
+                case CSV:
+                    jasperReportBuilder.toCsv(reportOutputStream);
+                    break;
+            }
+        } catch (DRException e) {
+            throw new RuntimeException("DRException in building jasperReportBuilder ", e);
+        }
+
         return reportOutputStream;
     }
 }
--- wavemaker-app-runtime-core/src/main/java/com/wavemaker/runtime/export/ExportType.java
+++ wavemaker-app-runtime-core/src/main/java/com/wavemaker/runtime/export/ExportType.java
@@ -1,40 +1,11 @@
 package com.wavemaker.runtime.export;
 
 
-import java.io.OutputStream;
-
-import net.sf.dynamicreports.jasper.builder.JasperReportBuilder;
-import net.sf.dynamicreports.report.exception.DRException;
-
 /**
  * @author <a href="mailto:anusha.dharmasagar@wavemaker.com">Anusha Dharmasagar</a>
  * @since 9/5/16
  */
 public enum ExportType {
-
-
-    EXCEL {
-        @Override
-        public JasperReportBuilder formatReport(JasperReportBuilder jasperReportBuilder, OutputStream reportOutputStream) {
-            try {
-                return jasperReportBuilder.toExcelApiXls(reportOutputStream);
-            } catch (DRException e) {
-                throw new RuntimeException("DRException in building jasperReportBuilder ", e);
-            }
-        }
-    },
-    CSV {
-        @Override
-        public JasperReportBuilder formatReport(JasperReportBuilder jasperReportBuilder, OutputStream reportOutputStream) {
-            try {
-                return jasperReportBuilder.toCsv(reportOutputStream);
-            } catch (DRException e) {
-                throw new RuntimeException("DRException in building jasperReportBuilder ", e);
-            }
-        }
-    };
-
-    public abstract JasperReportBuilder formatReport(JasperReportBuilder jasperReportBuilder, OutputStream reportOutputStream);
-
-
+    EXCEL,
+    CSV
 }
