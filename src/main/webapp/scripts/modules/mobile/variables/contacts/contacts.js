2f6d33aed2c2f707bf9ea3e9eac512bbaf6226d6
Bandhavya <bandhavya.vakiti@wavemaker.com>
2016-06-08 19:03:31 +0530

    Fix for WMS-6926: Display name is not getting for Contacts device variable in iPhone device
    Change-Id: I98ea72495dade0a45cdb5e215cd58b08def87cb2
    (cherry picked from commit d44cf02481ec5257ad7adfd9f51123b2fa05fa70)

--- src/main/webapp/scripts/modules/mobile/variables/contacts/contacts.js
+++ src/main/webapp/scripts/modules/mobile/variables/contacts/contacts.js
@@ -33,8 +33,21 @@ WM.module('wm.variables').run(['DeviceVariableService', '$cordovaContacts', func
                             multiple : true,
                             fields:  ['displayName']
                         };
+
+                    function getName(c) {
+                        var name = c.displayName;
+                        // In IOS, displayName is undefined, so using the formatted name.
+                        if (!name || name === "") {
+                            if (c.name.formatted) {
+                                return c.name.formatted;
+                            }
+                        }
+                        return name;
+                    }
+
                     $cordovaContacts.find(findOptions).then(function (data) {
                         data = _.filter(data, function (c) {
+                            c.displayName = getName(c);
                             return c.phoneNumbers && c.phoneNumbers.length > 0;
                         });
                         success(data);
