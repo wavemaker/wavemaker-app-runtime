94a6402aa22d9a843da0aedf9e1c6ea466e7fac1
vinay <vinay.kaparthi@wavemaker.com>
2016-07-15 15:33:00 +0530

    Fix for issue: Lightbox prefab is not working when placed inside a livelist.
    (lightbox listens for the click events on document and click events are prevented from propagation at live list)
    
    Change-Id: Id73c820dd4abcb6dd95c55acd44f567ea03ced8c
    (cherry picked from commit 0f199b66fd7c91bce48c397ca273661c1db9814a)

--- src/main/webapp/scripts/modules/widgets/live/list/liveList.js
+++ src/main/webapp/scripts/modules/widgets/live/list/liveList.js
@@ -887,10 +887,9 @@ WM.module('wm.widgets.live')
                 // listen on to the click event for the ul element & get li clicked of the live-list
                 $el.on('click.wmActive', 'ul.app-livelist-container', function (evt) {
                     // returning if click event is triggered within 50ms after pressup event occurred
-                    if (pressStartTimeStamp + 50 > Date.now() || WM.element(evt.target).is('input')) {
+                    if (pressStartTimeStamp + 50 > Date.now() || WM.element(evt.target).is('input, textarea, select')) {
                         return;
                     }
-                    evt.stopPropagation();
                     var $li = WM.element(evt.target).closest('li.app-list-item'),
                         $liScope = $li && $li.scope(),
                         isActive = $li.hasClass('active'),
