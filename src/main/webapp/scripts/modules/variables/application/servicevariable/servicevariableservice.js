441576f577053b0ea279a1cfa2c396d6a9f3cc59
Sumanth <sumanth.kodumuru@wavemaker.com>
2016-09-13 17:13:19 +0530

    Variables: Trigger methods on variables inside timeout
    Change-Id: I252745fc11c7ac81d5c58c76792e51318cad1b66

--- src/main/webapp/scripts/modules/variables/application/servicevariable/servicevariableservice.js
+++ src/main/webapp/scripts/modules/variables/application/servicevariable/servicevariableservice.js
@@ -685,20 +685,23 @@ wm.variables.services.$servicevariable = ['Variables',
                 }, forceReload);
             },
             update = function (options, success, error) {
-                var name = this.name;
-                options = options || {};
-                options.scope = this.activeScope || options.scope;
-                methods.getData(this, options, function (response) {
-                    if (CONSTANTS.isRunMode) {
-                        $rootScope.$emit('toggle-variable-state', name, false, response);
-                    }
-                    Utils.triggerFn(success, response);
-                }, function (errMsg) {
-                    if (CONSTANTS.isRunMode) {
-                        $rootScope.$emit('toggle-variable-state', name, false);
-                    }
-                    Utils.triggerFn(error, errMsg);
-                });
+                $timeout(function () {
+                    var name = this.name;
+                    options = options || {};
+                    options.scope = this.activeScope || options.scope;
+                    methods.getData(this, options, function (response) {
+                        if (CONSTANTS.isRunMode) {
+                            $rootScope.$emit('toggle-variable-state', name, false, response);
+                        }
+                        Utils.triggerFn(success, response);
+                    }, function (errMsg) {
+                        if (CONSTANTS.isRunMode) {
+                            $rootScope.$emit('toggle-variable-state', name, false);
+                        }
+                        Utils.triggerFn(error, errMsg);
+                    });
+                }.bind(this));
+
             },
         /* properties of a service variable - should contain methods applicable on this particular object */
             methods = {
