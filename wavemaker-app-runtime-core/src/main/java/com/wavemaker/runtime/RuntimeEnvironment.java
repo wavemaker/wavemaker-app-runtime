83be9e73ba16f7e4c356a446a5105fc06aac4745
Uday <uday.adepu@wavemaker.com>
2016-10-24 16:57:38 +0530

    Searching for wmapp.environment in System environment properties as well
    Change-Id: I65dfddb15434482e2b922ae6e2a83d41a24af118

--- wavemaker-app-runtime-core/src/main/java/com/wavemaker/runtime/RuntimeEnvironment.java
+++ wavemaker-app-runtime-core/src/main/java/com/wavemaker/runtime/RuntimeEnvironment.java
@@ -7,10 +7,13 @@ import org.apache.commons.lang3.StringUtils;
  */
 public class RuntimeEnvironment {
 
+    private static final String WMAPP_ENVIRONMENT_KEY="wmapp.environment";
+
     private static final String environment;
 
     static {
-        String property = System.getProperty("wmapp.environment");
+        String property = System.getProperty(WMAPP_ENVIRONMENT_KEY);
+        property = (StringUtils.isNotBlank(property)) ? property : System.getenv(WMAPP_ENVIRONMENT_KEY);
         environment = (StringUtils.isNotBlank(property)) ? property : "production";
     }
 
