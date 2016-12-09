a7bdba7ad6f761689c45d254931b8d8bfd4bcbf9
Arjun <arjun.sahasranam@wavemaker.com>
2016-12-09 16:23:12 +0530

    SAML IMPL: Load Idp Metadata from unsecured connection
    Change-Id: I205485c47f027cf229c04c833f636c9f34f682ac
    (cherry picked from commit 096889a1948c89d4fb2aad8baf855940cdfcfe90)

---
+++ wavemaker-app-runtime-core/src/main/java/com/wavemaker/runtime/security/provider/saml/SAMLConstants.java
@@ -0,0 +1,9 @@
+package com.wavemaker.runtime.security.provider.saml;
+
+/**
+ * Created by ArjunSahasranam on 12/12/16.
+ */
+public class SAMLConstants {
+    public static final String IDP_METADATA_XML = "idpMetadata.xml";
+
+}
