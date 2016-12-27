5d6d93d1801da49e5ed4e9f47ed15c81dfd0838c
Ravali <ravali.koppaka@wavemaker.com>
2016-12-27 16:51:02 +0530

    Added changes for providing APIs to reads procedures and sequences from db
    Change-Id: Id3aef5b6ae86862e2d199882561154a39f1c1993

--- wavemaker-app-runtime-core/src/main/java/com/wavemaker/runtime/data/model/procedures/ProcedureParameterType.java
+++ wavemaker-app-runtime-core/src/main/java/com/wavemaker/runtime/data/model/procedures/ProcedureParameterType.java
@@ -1,5 +1,9 @@
 package com.wavemaker.runtime.data.model.procedures;
 
+import java.util.Map;
+
+import org.springframework.util.LinkedCaseInsensitiveMap;
+
 /**
  * @author <a href="mailto:dilip.gundu@wavemaker.com">Dilip Kumar</a>
  * @since 5/10/16
@@ -19,11 +23,23 @@ public enum ProcedureParameterType {
     },
     IN_OUT;
 
+    private static final Map<String, ProcedureParameterType> dbValueVsProcedureParamType = new LinkedCaseInsensitiveMap<>();
+
+    static {
+        dbValueVsProcedureParamType.put("IN", ProcedureParameterType.IN);
+        dbValueVsProcedureParamType.put("OUT", ProcedureParameterType.OUT);
+        dbValueVsProcedureParamType.put("INOUT", ProcedureParameterType.IN_OUT);
+    }
+
     public boolean isOutParam() {
         return true;
     }
 
     public boolean isInParam() {
         return true;
+    }
+
+    public static ProcedureParameterType fromDB(String dbValue) {
+        return dbValueVsProcedureParamType.get(dbValue);
     }
 }
