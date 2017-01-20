68bc2f99a23eef5da1114f80bb713c10e4a1b44d
Arjun <arjun.sahasranam@wavemaker.com>
2017-01-20 16:52:37 +0530

    SAML KeyStoreUtils refactor
    Change-Id: Ic3e775df1c26a302d3915d12395ba1c7d3e75067

--- wavemaker-app-runtime-core/src/main/java/com/wavemaker/runtime/security/provider/saml/SAMLConstants.java
+++ wavemaker-app-runtime-core/src/main/java/com/wavemaker/runtime/security/provider/saml/SAMLConstants.java
@@ -9,6 +9,8 @@ public class SAMLConstants {
     public static final String SAML_SINGLE_LOGOUT = "/saml/SingleLogout";
     public static final String SAML_SSO = "/saml/SSO";
 
+    public static final String IDP_KEY = "idpkey";
+
     public static final String URN_OASIS_NAMES_TC_SAML_2_0_PROTOCOL = "urn:oasis:names:tc:SAML:2.0:protocol";
 
 
