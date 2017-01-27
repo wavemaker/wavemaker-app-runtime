a456ecf8e70c9d5db9e2eaeb63a412f5fa57d347
Punith <punith.mitra@wavemaker.com>
2017-01-27 19:11:15 +0530

    LiveList : aligning loading message to center and allowing loading message and spinner icon to show up simultaneously.
    Change-Id: I164b98468d4bdb0a9cd6f856334832603ac11143

--- src/main/webapp/scripts/modules/widgets/base/Base.js
+++ src/main/webapp/scripts/modules/widgets/base/Base.js
@@ -1770,7 +1770,7 @@ WM.module('wm.widgets.base', [])
                         "showcount": {"type": "boolean", "show": false},
                         "disableitem": {"type": "boolean", "bindable": "in-bound", "value": false},
                         "ondemandmessage": {"type": "string", "bindable": "in-bound", "showPrettyExprInDesigner": true, "value": "Load More"},
-                        "loadingicon": {"type": "string", "widget": "select-icon", "bindable": "in-bound", "value": "", "pattern": classRegex},
+                        "loadingicon": {"type": "string", "widget": "select-icon", "bindable": "in-bound", "value": "fa fa-circle-o-notch", "pattern": classRegex},
                         "paginationclass": {"type": "string", "pattern": classRegex, "widget": "list-picker", "options": ["pagination-sm", "pagination-lg", "btn-default", "btn-primary", "btn-info", "btn-warning", "btn-success", "btn-danger", "btn-inverse", "btn-lg", "btn-sm", "btn-xs", "btn-raised", "btn-fab", "btn-link", "btn-transparent", "jumbotron"]}
                     },
                     "wm.medialist": {
