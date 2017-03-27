821473adca97d06321f201c276fadf84549bc3e8
Kishore Routhu <kishore.routhu@wavemaker.com>
2017-03-27 16:21:47 +0530

    Refactored PrefabWebContentServlet to handling http error response
    Change-Id: Ic35e27bfe76b84b8bbe68527cfef8ea96a0a014d

--- wavemaker-app-runtime-prefab/src/main/java/com/wavemaker/runtime/prefab/web/PrefabWebContentServlet.java
+++ wavemaker-app-runtime-prefab/src/main/java/com/wavemaker/runtime/prefab/web/PrefabWebContentServlet.java
@@ -3,6 +3,7 @@ package com.wavemaker.runtime.prefab.web;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.file.InvalidPathException;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -10,7 +11,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.wavemaker.commons.WMRuntimeException;
+import com.wavemaker.commons.ResourceNotFoundException;
 import com.wavemaker.commons.util.IOUtils;
 
 /**
@@ -21,11 +22,16 @@ public class PrefabWebContentServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String requestURL = getRequestURL(request);
-        String prefabResourcePath = getPrefabResourcePath(requestURL);
-
-        InputStream resourceStream = readResource(request, prefabResourcePath);
-        OutputStream outputStream = response.getOutputStream();
-        IOUtils.copy(resourceStream, outputStream, true, false);
+        try {
+            String prefabResourcePath = getPrefabResourcePath(requestURL);
+            InputStream resourceStream = readResource(request, prefabResourcePath);
+            OutputStream outputStream = response.getOutputStream();
+            IOUtils.copy(resourceStream, outputStream, true, false);
+        } catch (ResourceNotFoundException rnfe) {
+            response.sendError(HttpServletResponse.SC_NOT_FOUND, rnfe.getMessage());
+        } catch (InvalidPathException ipe) {
+            response.sendError(HttpServletResponse.SC_NOT_FOUND, ipe.getMessage());
+        }
     }
 
     private String getRequestURL(HttpServletRequest request) {
@@ -34,13 +40,13 @@ public class PrefabWebContentServlet extends HttpServlet {
         return requestURL.substring(requestURL.lastIndexOf(contextPath) + contextPath.length());
     }
 
-    private static String getPrefabResourcePath(String requestURL) {
+    private static String getPrefabResourcePath(String requestURL) throws InvalidPathException {
         int startIndex =  "/app/prefabs/".length();
         String prefabResourcePath = requestURL.substring(startIndex);
 
         int endIndex = prefabResourcePath.indexOf("/");
         if (endIndex == -1) {
-            throw new WMRuntimeException("Invalid path of requested resource " + prefabResourcePath);
+            throw new InvalidPathException(prefabResourcePath, "Invalid resouce path " );
         }
 
         String prefabName = prefabResourcePath.substring(0, endIndex);
@@ -48,12 +54,12 @@ public class PrefabWebContentServlet extends HttpServlet {
         return "WEB-INF/prefabs/" + prefabName + "/webapp" + resourcePath;
     }
 
-    private InputStream readResource(HttpServletRequest request, String resourcePath) {
+    private InputStream readResource(HttpServletRequest request, String resourcePath) throws ResourceNotFoundException {
         ServletContext context = request.getSession().getServletContext();
         InputStream inputStream = context.getResourceAsStream(resourcePath);
 
         if (inputStream == null) {
-            throw new WMRuntimeException("Resource not found at " + resourcePath);
+            throw new ResourceNotFoundException("Requested resource " + resourcePath + "not found");
         }
         return inputStream;
     }
