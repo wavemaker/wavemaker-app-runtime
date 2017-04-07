ecc6e4426ee865ccc5f8e998f98d10bcc8fae7b2
Dilip <dilip.gundu@wavemaker.com>
2017-04-07 20:45:26 +0530

    WMS-9818# Handling oracle TIMESTAMP type while converting db type to java type.
    Change-Id: I97828808da8913e9c99bc62207d2f4621ea27572

--- wavemaker-app-runtime-core/src/main/java/com/wavemaker/runtime/data/converters/TimestampTypeConverter.java
+++ wavemaker-app-runtime-core/src/main/java/com/wavemaker/runtime/data/converters/TimestampTypeConverter.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -18,12 +18,16 @@ package com.wavemaker.runtime.data.converters;
 import java.sql.Timestamp;
 
 import org.hibernate.type.TimestampType;
+import org.springframework.util.ClassUtils;
+import org.springframework.util.ReflectionUtils;
 
 /**
  * @author <a href="mailto:dilip.gundu@wavemaker.com">Dilip Kumar</a>
  * @since 12/1/17
  */
 public class TimestampTypeConverter extends HibernateBackedJavaTypeConverter {
+
+    public static final String ORACLE_SQL_TIMESTAMP = "oracle.sql.TIMESTAMP";
 
     public TimestampTypeConverter() {
         super(TimestampType.INSTANCE.getJavaTypeDescriptor());
@@ -38,5 +42,23 @@ public class TimestampTypeConverter extends HibernateBackedJavaTypeConverter {
         } else {
             return super.fromString(value);
         }
+    }
+
+    @Override
+    public Object fromDbValue(final Object value) {
+        Object convertedValue = value;
+        if (value != null && ORACLE_SQL_TIMESTAMP.equals(value.getClass().getCanonicalName())) {
+            try {
+                Class<?> oracleTimestamp = ClassUtils.forName(ORACLE_SQL_TIMESTAMP, Thread.currentThread()
+                        .getContextClassLoader());
+                convertedValue = ReflectionUtils.invokeMethod(ClassUtils.getMethod(oracleTimestamp, "timestampValue"),
+                        value);
+            } catch (ClassNotFoundException e) {
+                // ignore this exception
+            }
+        } else {
+            convertedValue = super.fromDbValue(value);
+        }
+        return convertedValue;
     }
 }
