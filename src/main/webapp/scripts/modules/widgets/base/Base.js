31e3f099c5e35df236b760d627eea62f8c88f27c
Sudheer <sudheer.kavalakuntla@wavemaker.com>
2017-04-11 12:20:03 +0530

    Fix for WMS-11149: Mobile - Step title UI is not proper when show property is set to false for Wizard widget
    Change-Id: I03a5b367b579cb454d1512343f333562692c95bd

--- src/main/webapp/scripts/modules/widgets/base/Base.js
+++ src/main/webapp/scripts/modules/widgets/base/Base.js
@@ -2667,7 +2667,7 @@ WM.module('wm.widgets.base', [])
 
                             //Special handling for wizardstep to add just icon and content in ::before
                             if ($is.widgettype === 'wm-wizardstep') {
-                                $targetEl.removeClass('wm-hidden-overlay');
+                                $targetEl.removeClass('wm-hidden-overlay ' + $is.widgettype);
                             } else {
                                 $hiddenEl = $targetEl.find('.wm-hidden-overlay');
                                 $hiddenEl.remove();
@@ -2675,7 +2675,7 @@ WM.module('wm.widgets.base', [])
                         } else {
 
                             if ($is.widgettype === 'wm-wizardstep') {
-                                $targetEl.addClass('wm-hidden-overlay');
+                                $targetEl.addClass('wm-hidden-overlay ' + $is.widgettype);
                             } else {
                                 $targetEl.append($hiddenEleNode);
                             }
