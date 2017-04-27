f9ec024be93d38df619e7c23d483071e77b891a8
Kishore Routhu <kishore.routhu@wavemaker.com>
2017-04-27 16:05:01 +0530

    Fixing 404 for on prefabs config.json for empty context path
    Change-Id: Ic79436176fc34348fe97be73738ffb088fd0b9af

--- wavemaker-app-runtime-core/src/main/java/com/wavemaker/runtime/web/servlet/PrefabWebContentServlet.java
+++ wavemaker-app-runtime-core/src/main/java/com/wavemaker/runtime/web/servlet/PrefabWebContentServlet.java
@@ -1,50 +1,54 @@
 package com.wavemaker.runtime.web.servlet;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.nio.file.InvalidPathException;
 
 import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.wavemaker.commons.ResourceNotFoundException;
-import com.wavemaker.commons.util.IOUtils;
+import org.apache.commons.httpclient.HttpStatus;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.wavemaker.commons.util.HttpRequestUtils;
 
 /**
  * Created by kishore on 24/3/17.
  */
 public class PrefabWebContentServlet extends HttpServlet {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(PrefabWebContentServlet.class);
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String requestURL = getRequestURL(request);
-        String prefabResourceUrl = getPrefabResourcePath(requestURL);
-        RequestDispatcher requestDispatcher = getServletContext().getRequestDispatcher(prefabResourceUrl);
-        requestDispatcher.forward(request, response);
-    }
+        String prefabResourcePath = request.getPathInfo();
+        if (StringUtils.isBlank(prefabResourcePath)) {
+            writeErrorResponse(request, response, prefabResourcePath);
+            return;
+        }
 
-    private String getRequestURL(HttpServletRequest request) {
-        StringBuffer requestURL = request.getRequestURL();
-        String contextPath = request.getContextPath();
-        return requestURL.substring(requestURL.lastIndexOf(contextPath) + contextPath.length());
-    }
-
-    private static String getPrefabResourcePath(String requestURL) throws InvalidPathException {
-        int startIndex =  "/app/prefabs/".length();
-        String prefabResourcePath = requestURL.substring(startIndex);
+        if (prefabResourcePath.startsWith("/")) {
+            prefabResourcePath = prefabResourcePath.substring(1);
+        }
 
         int endIndex = prefabResourcePath.indexOf("/");
         if (endIndex == -1) {
-            throw new InvalidPathException(prefabResourcePath, "Invalid resouce path " );
+            writeErrorResponse(request, response, prefabResourcePath);
+            return;
         }
 
         String prefabName = prefabResourcePath.substring(0, endIndex);
-        String resourcePath = prefabResourcePath.substring(prefabName.length());
-        return "/WEB-INF/prefabs/" + prefabName + "/webapp" + resourcePath;
+        String resourceRelativePath = prefabResourcePath.substring(prefabName.length());
+        String prefabResourceUpdatedPath = "/WEB-INF/prefabs/" + prefabName + "/webapp" + resourceRelativePath;
+        RequestDispatcher requestDispatcher = getServletContext().getRequestDispatcher(prefabResourceUpdatedPath);
+        requestDispatcher.forward(request, response);
+    }
+
+    private void writeErrorResponse(HttpServletRequest request, HttpServletResponse response, String prefabResourcePath) throws IOException {
+        LOGGER.warn("Invalid prefab uri {} received", request.getRequestURI());
+        HttpRequestUtils.writeJsonErrorResponse("Invalid prefab url " + request.getRequestURI(), HttpStatus.SC_BAD_REQUEST, response);
     }
 }
