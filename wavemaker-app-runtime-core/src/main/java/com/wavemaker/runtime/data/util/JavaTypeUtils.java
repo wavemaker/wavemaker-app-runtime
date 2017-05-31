a05164846fe76cc56ff1b24c5987570e37c5f482
Anusha Dharmasagar <anusha.dharmasagar@wavemaker.com>
2017-05-31 17:32:07 +0530

    WMS-11705# Handling primitive types also in known types check
    Change-Id: I92f3134097dfa1a8571fa5a49debda66de24ddd7

--- wavemaker-app-runtime-core/src/main/java/com/wavemaker/runtime/data/util/JavaTypeUtils.java
+++ wavemaker-app-runtime-core/src/main/java/com/wavemaker/runtime/data/util/JavaTypeUtils.java
@@ -2,6 +2,7 @@ package com.wavemaker.runtime.data.util;
 
 import java.util.Collection;
 
+import org.apache.commons.lang3.ClassUtils;
 import org.hibernate.HibernateException;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
@@ -17,27 +18,9 @@ public class JavaTypeUtils {
     private static MultiValueMap<String, JavaType> classNameVsJavaTypeMap = new LinkedMultiValueMap<>();
 
     static {
-        classNameVsJavaTypeMap.add(JavaType.BYTE.getClassName(), JavaType.BYTE);
-        classNameVsJavaTypeMap.add(JavaType.SHORT.getClassName(), JavaType.SHORT);
-        classNameVsJavaTypeMap.add(JavaType.INTEGER.getClassName(), JavaType.INTEGER);
-        classNameVsJavaTypeMap.add(JavaType.LONG.getClassName(), JavaType.LONG);
-        classNameVsJavaTypeMap.add(JavaType.BIG_INTEGER.getClassName(), JavaType.BIG_INTEGER);
-        classNameVsJavaTypeMap.add(JavaType.FLOAT.getClassName(), JavaType.FLOAT);
-        classNameVsJavaTypeMap.add(JavaType.DOUBLE.getClassName(), JavaType.DOUBLE);
-        classNameVsJavaTypeMap.add(JavaType.BIG_DECIMAL.getClassName(), JavaType.BIG_DECIMAL);
-        classNameVsJavaTypeMap.add(JavaType.BOOLEAN.getClassName(), JavaType.BOOLEAN);
-        classNameVsJavaTypeMap.add(JavaType.YES_OR_NO.getClassName(), JavaType.YES_OR_NO);
-        classNameVsJavaTypeMap.add(JavaType.TRUE_OR_FALSE.getClassName(), JavaType.TRUE_OR_FALSE);
-        classNameVsJavaTypeMap.add(JavaType.CHARACTER.getClassName(), JavaType.CHARACTER);
-        classNameVsJavaTypeMap.add(JavaType.STRING.getClassName(), JavaType.STRING);
-        classNameVsJavaTypeMap.add(JavaType.TEXT.getClassName(), JavaType.TEXT);
-        classNameVsJavaTypeMap.add(JavaType.CLOB.getClassName(), JavaType.CLOB);
-        classNameVsJavaTypeMap.add(JavaType.BLOB.getClassName(), JavaType.BLOB);
-        classNameVsJavaTypeMap.add(JavaType.DATE.getClassName(), JavaType.DATE);
-        classNameVsJavaTypeMap.add(JavaType.TIME.getClassName(), JavaType.TIME);
-        classNameVsJavaTypeMap.add(JavaType.DATETIME.getClassName(), JavaType.DATETIME);
-        classNameVsJavaTypeMap.add(JavaType.TIMESTAMP.getClassName(), JavaType.TIMESTAMP);
-        classNameVsJavaTypeMap.add(JavaType.CURSOR.getClassName(), JavaType.CURSOR);
+        for (final JavaType javaType : JavaType.values()) {
+            classNameVsJavaTypeMap.add(javaType.getClassName(), javaType);
+        }
     }
 
     public static Object convert(String toClass, Object value) {
@@ -58,8 +41,9 @@ public class JavaTypeUtils {
     }
 
     public static boolean isKnownType(Class<?> type) {
+        type = ClassUtils.primitiveToWrapper(type);
         final String typeName = type.getCanonicalName();
-        return classNameVsJavaTypeMap.containsKey(typeName);
+        return (classNameVsJavaTypeMap.containsKey(typeName));
     }
 
     public static boolean isNotCollectionType(final Class<?> typeClass) {
