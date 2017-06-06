6dc4a5f2e93e1879385f07cd6c5642bc5b600b88
“PunithMithra” <punith.mitra@wavemaker.com>
2017-06-06 15:05:08 +0530

    List: Selection item on reorder is not persisting on reorder.
    Change-Id: Ifbf869510119568ed7f1821425ca4bb46aa330fd

--- src/main/webapp/scripts/modules/widgets/live/list/liveList.js
+++ src/main/webapp/scripts/modules/widgets/live/list/liveList.js
@@ -1353,9 +1353,9 @@ WM.module('wm.widgets.live')
                         };
                         Utils.triggerFn($is.onReorder, {$event: evt, $data: data, $changedItem: changedItem});
                         $dragEl.removeData('oldIndex');
+                        $rs.$safeApply($is);
                         // to persist the selected items after reorder.
                         $is.selecteditem = Utils.getClonedObject($is.selecteditem);
-                        $rs.$safeApply($is);
                     }
                 });
                 $el.find('.app-livelist-container').droppable({'accept': '.app-list-item'});
