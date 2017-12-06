4b4ab15fb84778aecc28d62bbf449662122ba77e
Srujan <srujan.thammisetty@wavemaker.com>
2017-12-06 16:46:53 +0530

    prefab : setting  config location to prefab-services.xml file.
    Change-Id: Ic992fc0d2eac1ed51dc59b52610670ee16a5308d

--- wavemaker-app-runtime-prefab/src/main/java/com/wavemaker/runtime/prefab/context/PrefabWebApplicationContext.java
+++ wavemaker-app-runtime-prefab/src/main/java/com/wavemaker/runtime/prefab/context/PrefabWebApplicationContext.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -35,7 +35,7 @@ public class PrefabWebApplicationContext extends XmlWebApplicationContext {
         setClassLoader(prefabClassLoader);
         setServletContext(servletContext);
         setDisplayName("Prefab Context [" + prefab.getName() + "]");
-        setConfigLocations(new String[]{"classpath:" + prefab.getName() + "-prefab-services.xml"});
+        setConfigLocations(new String[]{"classpath:prefab-services.xml"});
         ClassLoader currentLoader = Thread.currentThread().getContextClassLoader();
         try {
             Thread.currentThread().setContextClassLoader(prefabClassLoader);
