abb27e6ca71a957d470ba88e5b49184d8f32b28e
Kishore Routhu <kishore.routhu@wavemaker.com>
2018-01-31 10:56:51 +0530

    Fixing page redirection issue after SAML login
    Change-Id: If6cd4b6ad1b1935f0b43b10d7c21f1831e28e816

--- wavemaker-app-runtime-core/src/main/java/com/wavemaker/runtime/security/provider/saml/WMSAMLEntryPoint.java
+++ wavemaker-app-runtime-core/src/main/java/com/wavemaker/runtime/security/provider/saml/WMSAMLEntryPoint.java
@@ -16,6 +16,7 @@
 package com.wavemaker.runtime.security.provider.saml;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 
 import javax.servlet.ServletException;
@@ -23,13 +24,17 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.lang3.StringUtils;
+import org.opensaml.saml2.metadata.provider.MetadataProviderException;
+import org.opensaml.ws.transport.http.HttpServletRequestAdapter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.saml.SAMLEntryPoint;
+import org.springframework.security.saml.context.SAMLMessageContext;
 import org.springframework.security.saml.websso.WebSSOProfileOptions;
 
+import com.wavemaker.commons.WMRuntimeException;
 import com.wavemaker.runtime.util.HttpRequestUtils;
 
 import static com.wavemaker.runtime.security.SecurityConstants.SESSION_NOT_FOUND;
@@ -56,34 +61,47 @@ public class WMSAMLEntryPoint extends SAMLEntryPoint {
             response.setHeader(X_WM_LOGIN_ERROR_MESSAGE, SESSION_NOT_FOUND);
             response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
         } else {
-            if (SAMLConfig.ValidateType.RELAXED == samlConfig.getValidateType()) {
-                // setting appruntimePath only in the case of DEV mode.
-
-                StringBuffer requestURL = request.getRequestURL();
-                String relayState = defaultOptions.getRelayState();
-                logger.debug("Request URL is {} and RelayState is {}", requestURL.toString(), relayState);
-
-                // setting app-runtime path in relay state.
-                if (StringUtils.isBlank(relayState)) {
-                    URL incomingRequestUrl = new URL(requestURL.toString());
-                    String incomingRequestUrlPath = incomingRequestUrl.getPath(); //content after port,
-                    // excluding the query string, but starts with slash (/)
-
-                    int indexOfPath = requestURL.indexOf(incomingRequestUrlPath);
-                    StringBuffer requestUrlBeforePath = requestURL.delete(indexOfPath, requestURL.length());
-
-                    String[] partsInPath = StringUtils.split(incomingRequestUrlPath, "/");
-
-                    // taking first 2 parts from incomingRequestUrlPath. viz. run-xxx id and app name.
-                    String appRuntimePath = partsInPath[0] + "/" + partsInPath[1];
-                    String appUrl = requestUrlBeforePath.toString() + "/" + appRuntimePath;
-                    logger.debug("URL incomingRequestUrlPath constructed for application is {}", appUrl);
-
-                    defaultOptions.setRelayState(appUrl);
-                    webSSOProfileOptions.setRelayState(appUrl);
-                }
-            }
             super.commence(request, response, e);
         }
+    }
+
+    @Override
+    protected WebSSOProfileOptions getProfileOptions(SAMLMessageContext context, AuthenticationException exception) throws MetadataProviderException {
+        WebSSOProfileOptions profileOptions = super.getProfileOptions(context, exception);
+
+        HttpServletRequestAdapter requestAdapter = (HttpServletRequestAdapter) context.getInboundMessageTransport();
+        HttpServletRequest request = requestAdapter.getWrappedRequest();
+        StringBuffer requestURL = request.getRequestURL();
+        logger.debug("Request URL is {} and RelayState is {}", requestURL.toString());
+
+        try {
+            URL incomingRequestUrl = new URL(requestURL.toString());
+            String incomingRequestUrlPath = incomingRequestUrl.getPath(); //content after port,
+            // excluding the query string, but starts with slash (/)
+
+            int indexOfPath = requestURL.indexOf(incomingRequestUrlPath);
+            StringBuffer requestUrlBeforePath = requestURL.delete(indexOfPath, requestURL.length());
+
+            String[] partsInPath = StringUtils.split(incomingRequestUrlPath, "/");
+
+            // taking first 2 parts from incomingRequestUrlPath. viz. run-xxx id and app name.
+            String appRuntimePath = partsInPath[0];
+            if (SAMLConfig.ValidateType.RELAXED == samlConfig.getValidateType()) {
+                appRuntimePath = appRuntimePath.concat("/").concat(partsInPath[1]);
+            }
+            String appUrl = requestUrlBeforePath.toString().concat("/").concat(appRuntimePath);
+            logger.debug("URL incomingRequestUrlPath constructed for application is {}", appUrl);
+
+            String redirectPage = request.getParameter("redirectPage");
+            if (StringUtils.isNotEmpty(redirectPage) && StringUtils.isNotEmpty(appUrl) && !StringUtils
+                    .containsAny(appUrl, new char[]{'#', '?'})) {
+                appUrl = appUrl.concat("#").concat(redirectPage);
+            }
+            profileOptions.setRelayState(appUrl);
+        } catch (MalformedURLException e) {
+            logger.error("Invalid URL {}", requestURL.toString(), e);
+            throw new WMRuntimeException(e);
+        }
+        return profileOptions;
     }
 }
