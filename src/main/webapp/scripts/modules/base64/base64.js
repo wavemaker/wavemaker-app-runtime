aa85dde180b0703729c5b61b683bbea787f5f7a7
Vibhu <vibhu.singhal@wavemaker.com>
2018-03-23 19:15:47 +0530

    Base64: assign methods with reference to window. Fixes the illeagal invocation error
    Change-Id: I0c76beb0b8612241b7cd42bcdebf7028fb9bf683

--- src/main/webapp/scripts/modules/base64/base64.js
+++ src/main/webapp/scripts/modules/base64/base64.js
@@ -3,6 +3,6 @@
  * base64 encode/decode compatible with window.btoa/atob
  */
 angular.module('base64', []).constant('$base64', {
-    encode: window.btoa,
-    decode: window.atob
+    encode: window.btoa.bind(window),
+    decode: window.atob.bind(window)
 });
