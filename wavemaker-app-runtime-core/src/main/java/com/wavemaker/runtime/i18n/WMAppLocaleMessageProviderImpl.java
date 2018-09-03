c36af17b8e6fe86435f5b4d1d23b76c573f0565d
Srujan <srujan.thammisetty@wavemaker.com>
2018-09-03 15:29:54 +0530

    i18n: Replaced LocaleMessageProviderImpl with WMAppLocaleMessageProviderImpl.
    Change-Id: I831da78e2dc08c501274e57a01c99115264d95dc

---
+++ wavemaker-app-runtime-core/src/main/java/com/wavemaker/runtime/i18n/WMAppLocaleMessageProviderImpl.java
@@ -0,0 +1,38 @@
+package com.wavemaker.runtime.i18n;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.ResourcePatternResolver;
+
+import com.wavemaker.commons.WMError;
+import com.wavemaker.commons.i18n.LocaleData;
+import com.wavemaker.commons.i18n.LocaleMessageProviderImpl;
+import com.wavemaker.commons.json.JSONUtils;
+
+/**
+ * Created by srujant on 3/9/18.
+ */
+public class WMAppLocaleMessageProviderImpl extends LocaleMessageProviderImpl {
+
+    public WMAppLocaleMessageProviderImpl(List<String> locationPatterns, ResourcePatternResolver resourcePatternResolver) {
+        super(locationPatterns, resourcePatternResolver);
+    }
+
+    @Override
+    protected Map<String, String> loadLocaleMessages(String locale) {
+        Map<String, String> localeMessages = super.loadLocaleMessages(locale);
+        Resource[] resources = getResources("/resources/i18n/", locale + ".json");
+        for (Resource resource : resources) {
+            try {
+                LocaleData localeData = JSONUtils.toObject(resource.getInputStream(), LocaleData.class);
+                localeMessages.putAll(localeData.getMessages());
+            } catch (IOException e) {
+                throw new WMError("Failed to read locale resources for locale " + locale, e);
+            }
+        }
+        return localeMessages;
+    }
+}
