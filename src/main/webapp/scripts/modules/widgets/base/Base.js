ce4ff01730d422d0fd1279185e539d0b75530eb2
“PunithMithra” <punith.mitra@wavemaker.com>
2018-09-04 14:45:45 +0530

    Implementing locale for currency
    Change-Id: I8cf84192be6d913e00a6a23f7d9f654dd6f42a2e

--- src/main/webapp/scripts/modules/widgets/base/Base.js
+++ src/main/webapp/scripts/modules/widgets/base/Base.js
@@ -551,6 +551,7 @@ WM.module('wm.widgets.base', [])
                         "maxvalue": {"type": "number", "bindable": "in-bound"},
                         "readonly": {"type": "boolean", "bindable": "in-bound"},
                         "step": {"type": "number"},
+                        "updateon": {"type": "list", "value": "blur", "widget": "update-on"},
                         "currency": {"type": "list", "value": "USD", "options": ["AED", "AFN", "ALL", "AMD", "ARS", "AUD", "AZN", "BAM", "BDT", "BGN", "BHD", "BIF", "BND", "BOB", "BRL", "BWP", "BYR", "BZD", "CAD", "CDF", "CHF", "CLP", "CNY", "COP", "CRC", "CVE", "CZK", "DJF", "DKK", "DOP", "DZD", "EEK", "EGP", "ERN", "ETB", "EUR", "GBP", "GEL", "GHS", "GNF", "GTQ", "HKD", "HNL", "HRK", "HUF", "IDR", "ILS", "INR", "IQD", "IRR", "ISK", "JMD", "JOD", "JPY", "KES", "KHR", "KMF", "KRW", "KWD", "KZT", "LBP", "LKR", "LTL", "LVL", "LYD", "MAD", "MDL", "MGA", "MKD", "MMK", "MOP", "MUR", "MXN", "MYR", "MZN", "NAD", "NGN", "NIO", "NOK", "NPR", "NZD", "OMR", "PAB", "PEN", "PHP", "PKR", "PLN", "PYG", "QAR", "RON", "RSD", "RUB", "RWF", "SAR", "SDG", "SEK", "SGD", "SOS", "SYP", "THB", "TND", "TOP", "TRY", "TTD", "TWD", "TZS", "UAH", "UGX", "USD", "UYU", "UZS", "VEF", "VND", "XAF", "XOF", "YER", "ZAR", "ZMK"]},
                         /* Properties: help */
                         "placeholder": {"type": "string", "value": "Enter value", "bindable": "in-bound"},
