c7d00078d4df43fa582b2ac65922605b1351ac55
Dilip <dilip.gundu@wavemaker.com>
2018-10-05 18:50:48 +0530

    disabled wm jackson module w.r.t jackson anygetter serialization issue.
    Change-Id: I3ca0f42aa6aace25a6c8262cceab71d7c5087ce4

--- wavemaker-app-runtime-core/src/main/java/com/wavemaker/runtime/WMObjectMapper.java
+++ wavemaker-app-runtime-core/src/main/java/com/wavemaker/runtime/WMObjectMapper.java
@@ -54,7 +54,6 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.wavemaker.commons.json.deserializer.HttpHeadersDeSerializer;
 import com.wavemaker.commons.json.deserializer.WMDateDeSerializer;
 import com.wavemaker.commons.json.deserializer.WMSqlDateDeSerializer;
-import com.wavemaker.commons.json.module.WMJacksonModule;
 import com.wavemaker.commons.json.serializer.NoOpByteArraySerializer;
 import com.wavemaker.commons.json.serializer.WMLocalDateTimeSerializer;
 import com.wavemaker.runtime.mixins.SliceMixin;
@@ -330,7 +329,8 @@ public class WMObjectMapper extends ObjectMapper {
             javaTimeModule.addSerializer(LocalDateTime.class, new WMLocalDateTimeSerializer());
             registerModule(javaTimeModule);
 
-            registerModule(new WMJacksonModule(false));
+            // will add once anyGetter issue fixed.
+//            registerModule(new WMJacksonModule(false));
 
             setPropertyNamingStrategy(PROPERTY_NAMING_STRATEGY);
         }
