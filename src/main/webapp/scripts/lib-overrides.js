0efe4ea6ec6156c714530277174abe9e5b519326
“PunithMithra” <punith.mitra@wavemaker.com>
2018-11-09 10:53:29 +0530

    Added uppercase method on Angular object. Since in 7.5.x the method is deprecated.
    Change-Id: I1439c4941fa244d2d155333fdce2b4a89c718c8b

--- src/main/webapp/scripts/lib-overrides.js
+++ src/main/webapp/scripts/lib-overrides.js
@@ -6,4 +6,5 @@
     'use strict';
     // lowercase method on "angular" object is used by "textangular" library which is deprecated angular 1.7.5v.
     angular.lowercase = _.lowerCase;
+    angular.uppercase = _.upperCase;
 })();
