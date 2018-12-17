46f18cf308fc3e562cec86aedb1379678b63e523
Sujith <sujith.simon@wavemaker.com>
2018-12-17 13:13:01 +0530

    Updated Antlr parsing error for better understanding and debugging.
    Change-Id: I34fa2777773278c4bd347a2ed54cf3d8420c87ec
    (cherry picked from commit 6387fce2a0368938e6c572f85a299755ed0b9bec)

--- wavemaker-app-runtime-core/src/main/java/com/wavemaker/runtime/data/filter/parser/WMHqlAntlrErrorListner.java
+++ wavemaker-app-runtime-core/src/main/java/com/wavemaker/runtime/data/filter/parser/WMHqlAntlrErrorListner.java
@@ -3,6 +3,7 @@ package com.wavemaker.runtime.data.filter.parser;
 import org.antlr.v4.runtime.BaseErrorListener;
 import org.antlr.v4.runtime.RecognitionException;
 import org.antlr.v4.runtime.Recognizer;
+import org.antlr.v4.runtime.TokenStream;
 import org.antlr.v4.runtime.misc.ParseCancellationException;
 
 import com.wavemaker.commons.MessageResource;
@@ -13,8 +14,11 @@ public class WMHqlAntlrErrorListner extends BaseErrorListener {
     @Override
     public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol, int line, int charPositionInLine, String msg, RecognitionException e)
             throws ParseCancellationException {
+        String inputQuery = ((TokenStream) recognizer.getInputStream()).getTokenSource().getInputStream().toString();
+
+
         throw new HqlGrammarException(
-                MessageResource.create("Failed to parse at {}:{} due to {}."), line, charPositionInLine, msg
+                MessageResource.create("Syntax error in '{0}' at {1}."), inputQuery, charPositionInLine, msg
         );
     }
 }
