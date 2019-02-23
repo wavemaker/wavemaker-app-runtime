70650d4970c32136f6f0f9ed3a195188e41d39e2
Naresh <naresh.ravulapalli@wavemaker.com>
2019-02-23 19:23:44 +0530

    js errors fixed
    Change-Id: If88452b6b3f07170b038b312cd3632b85e16ef0a
    (cherry picked from commit 2fc72e08da82d8373c0b5f06cb3d6d7eefce739c)

--- src/main/webapp/scripts/utils.js
+++ src/main/webapp/scripts/utils.js
@@ -2667,15 +2667,18 @@ WM.module('wm.utils', [])
          * @returns {*}
          */
         function getCustomHeaderVal(headerName) {
+            var headerValue = null;
             switch (headerName) {
                 case CONSTANTS.CUSTOM_HTTP_HEADERS.WM_PLATFORM_VERSION:
                     if ($rootScope.project && $rootScope.project.platformVersion) {
-                        return $rootScope.project.platformVersion;
+                        headerValue = $rootScope.project.platformVersion;
                     }
                     break;
                 default:
-                    return null;
+                    //do nothing;
+                    break;
             }
+            return headerValue;
         }
 
         //Format value for datetime types
@@ -3235,7 +3238,7 @@ WM.module('wm.utils', [])
         this.isInsecureContentRequest   = isInsecureContentRequest;
         this.isValidAppServerUrl        = isValidAppServerUrl;
         this.addDefaultHeaders          = addDefaultHeaders;
-        this.getPlatformVersionHeader   = getPlatformVersionHeader;
+        this.getCustomHeaderVal         = getCustomHeaderVal;
         this.formatDate                 = formatDate;
         this.getBlob                    = getBlob;
         this.getMetaDataFromData        = getMetaDataFromData;
