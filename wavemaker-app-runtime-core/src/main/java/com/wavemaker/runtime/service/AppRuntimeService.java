140d2c84b17911a1b233f61c7ebcdfbbed8ce04e
Dilip <dilip.gundu@wavemaker.com>
2019-03-29 16:37:42 +0530

    1. Profilized hql run api in the runtime for restricting in deploy mode.
    Change-Id: I7d6e79bbd64a77f16c9681422e37b6c8913d1271
    (cherry picked from commit 5036c5facb41452b5ea69bd4f9219d1bfe8c3eda)

--- wavemaker-app-runtime-core/src/main/java/com/wavemaker/runtime/service/AppRuntimeService.java
+++ wavemaker-app-runtime-core/src/main/java/com/wavemaker/runtime/service/AppRuntimeService.java
@@ -2,15 +2,7 @@ package com.wavemaker.runtime.service;
 
 import java.io.InputStream;
 import java.util.Map;
-
 import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.data.domain.Pageable;
-import org.springframework.web.multipart.MultipartHttpServletRequest;
-
-import com.wavemaker.runtime.data.model.DesignServiceResponse;
-import com.wavemaker.runtime.data.model.procedures.RuntimeProcedure;
-import com.wavemaker.runtime.data.model.queries.RuntimeQuery;
 
 /**
  * Created by Kishore Routhu on 21/6/17 2:57 PM.
@@ -20,12 +12,6 @@ public interface AppRuntimeService {
     Map<String, Object> getApplicationProperties();
 
     String getApplicationType();
-
-    DesignServiceResponse testRunQuery(String serviceId, MultipartHttpServletRequest request, Pageable pageable);
-
-    DesignServiceResponse testRunProcedure(String serviceId, RuntimeProcedure procedure);
-
-    Object executeQuery(String serviceId, RuntimeQuery query, Pageable pageable);
 
     InputStream getValidations(HttpServletResponse response);
 }
--- wavemaker-app-runtime-core/src/main/java/com/wavemaker/runtime/service/AppRuntimeServiceImpl.java
+++ wavemaker-app-runtime-core/src/main/java/com/wavemaker/runtime/service/AppRuntimeServiceImpl.java
@@ -6,12 +6,9 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 import java.util.Set;
-
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Pageable;
-import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import com.wavemaker.commons.MessageResource;
 import com.wavemaker.commons.WMRuntimeException;
@@ -20,11 +17,7 @@ import com.wavemaker.commons.json.JSONUtils;
 import com.wavemaker.commons.util.PropertiesFileUtils;
 import com.wavemaker.commons.validations.DbValidationsConstants;
 import com.wavemaker.runtime.app.AppFileSystem;
-import com.wavemaker.runtime.data.model.DesignServiceResponse;
-import com.wavemaker.runtime.data.model.procedures.RuntimeProcedure;
-import com.wavemaker.runtime.data.model.queries.RuntimeQuery;
 import com.wavemaker.runtime.security.SecurityService;
-import com.wavemaker.runtime.util.MultipartQueryUtils;
 
 /**
  * Created by Kishore Routhu on 21/6/17 3:00 PM.
@@ -65,16 +58,17 @@ public class AppRuntimeServiceImpl implements AppRuntimeService {
             if (applicationProperties == null) {
                 InputStream inputStream = appFileSystem.getClasspathResourceStream(APP_PROPERTIES);
                 Properties properties = PropertiesFileUtils.loadFromXml(inputStream);
-                Map<String, Object> applicationProperties = new HashMap<>();
+                Map<String, Object> appProperties = new HashMap<>();
                 for (String s : uiProperties) {
-                    applicationProperties.put(s, properties.get(s));
+                    appProperties.put(s, properties.get(s));
                 }
                 if("APPLICATION".equals(getApplicationType())) {
-                    applicationProperties.put("securityEnabled", securityService.isSecurityEnabled());
-                    applicationProperties.put("xsrf_header_name", getCsrfHeaderName());
+                    appProperties.put("securityEnabled", securityService.isSecurityEnabled());
+                    appProperties.put("xsrf_header_name", getCsrfHeaderName());
                 }
-                applicationProperties.put("supportedLanguages", getSupportedLocales(appFileSystem.getWebappI18nLocaleFileNames()));
-                this.applicationProperties = applicationProperties;  
+                appProperties
+                        .put("supportedLanguages", getSupportedLocales(appFileSystem.getWebappI18nLocaleFileNames()));
+                this.applicationProperties = appProperties;
             }
         }
         return new HashMap<>(applicationProperties);
@@ -92,24 +86,6 @@ public class AppRuntimeServiceImpl implements AppRuntimeService {
     }
 
     @Override
-    public DesignServiceResponse testRunQuery(
-            String serviceId, MultipartHttpServletRequest request, Pageable pageable) {
-        RuntimeQuery query = MultipartQueryUtils.readContent(request, RuntimeQuery.class);
-        MultipartQueryUtils.setMultiparts(query.getParameters(), request.getMultiFileMap());
-        return queryDesignService.testRunQuery(serviceId, query, pageable);
-    }
-
-    @Override
-    public DesignServiceResponse testRunProcedure(String serviceId, RuntimeProcedure procedure) {
-        return procedureDesignService.testRunProcedure(serviceId, procedure);
-    }
-
-    @Override
-    public Object executeQuery(String serviceId, RuntimeQuery query, Pageable pageable) {
-        return queryDesignService.executeQuery(serviceId, query, pageable);
-    }
-
-    @Override
     public InputStream getValidations(HttpServletResponse httpServletResponse) {
         return appFileSystem.getWebappResource("WEB-INF/" + DbValidationsConstants.DB_VALIDATIONS_JSON_FILE);
     }
@@ -121,7 +97,7 @@ public class AppRuntimeServiceImpl implements AppRuntimeService {
     }
 
     private void addToLocaleMap(Map<String, Object> map, InputStream localeFileInputStream, String fileName) {
-        fileName = fileName.substring(fileName.lastIndexOf("/") + 1, fileName.lastIndexOf("."));
+        fileName = fileName.substring(fileName.lastIndexOf('/') + 1, fileName.lastIndexOf('.'));
         try {
             FinalLocaleData userLocaleData = JSONUtils.toObject(localeFileInputStream, FinalLocaleData.class);
             map.put(fileName, userLocaleData.getFiles());
