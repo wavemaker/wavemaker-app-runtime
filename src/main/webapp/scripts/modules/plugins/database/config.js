e41c76536a6a075cec205cb1b40985d9923a8014
sampathg9 <sampath.gembali@wavemaker.com>
2019-05-08 15:51:33 +0530

    WMS-16464: Added notin and notequalsignorecase filter options for live variables.
    Change-Id: I8f97d5c24fe73e77bd56469f507977a0d54cf57e

--- src/main/webapp/scripts/modules/plugins/database/config.js
+++ src/main/webapp/scripts/modules/plugins/database/config.js
@@ -653,8 +653,10 @@ wm.plugins.database.constant('DB_CONSTANTS', {
         "exact"            : "EQUALS",
         "exactignorecase"  : "EQUALS_IGNORECASE",
         "notequals"        : "NOT_EQUALS",
+        "notequalsignorecase": "NOT_EQUALS_IGNORECASE",
         "between"          : "BETWEEN",
         "in"               : "IN",
+        "notin"            : "NOT_IN",
         "lessthan"         : "LESS_THAN",
         "lessthanequal"    : "LESS_THAN_OR_EQUALS",
         "greaterthan"      : "GREATER_THAN",
@@ -676,8 +678,10 @@ wm.plugins.database.constant('DB_CONSTANTS', {
         "EQUALS"                 : "${0}=${1}",
         "EQUALS_IGNORECASE"      : "${0}=${1}",
         "NOT_EQUALS"             : "${0}!=${1}",
+        "NOT_EQUALS_IGNORECASE"  : "${0}!=${1}",
         "BETWEEN"                : "${0} between ${1}",
         "IN"                     : "${0} in ${1}",
+        "NOT_IN"                 : "${0} not in ${1}",
         "LESS_THAN"              : "${0}<${1}",
         "LESS_THAN_OR_EQUALS"    : "${0}<=${1}",
         "GREATER_THAN"           : "${0}>${1}",
@@ -689,7 +693,7 @@ wm.plugins.database.constant('DB_CONSTANTS', {
         "NULL_OR_EMPTY"          : "${0} is null or ${0}=''"
     },
     "DATABASE_EMPTY_MATCH_MODES": ["NULL", "IS_NOT_NULL", "EMPTY", "IS_NOT_EMPTY", "NULL_OR_EMPTY"],
-    "DATABASE_STRING_MODES": ["LIKE", "STARTING_WITH", "STARTING_WITH_IGNORECASE", "ENDING_WITH", "ENDING_WITH_IGNORECASE", "CONTAINING", "CONTAINING_IGNORECASE", "EQUALS", "EQUALS_IGNORECASE", "NOT_EQUALS"],
+    "DATABASE_STRING_MODES": ["LIKE", "STARTING_WITH", "STARTING_WITH_IGNORECASE", "ENDING_WITH", "ENDING_WITH_IGNORECASE", "CONTAINING", "CONTAINING_IGNORECASE", "EQUALS", "EQUALS_IGNORECASE", "NOT_EQUALS", "NOT_EQUALS_IGNORECASE"],
     "DATABASE_NULL_EMPTY_MATCH": {
         "NULL"          : "NULL",
         "IS_NOT_NULL"   : "IS_NOT_NULL",
@@ -697,7 +701,7 @@ wm.plugins.database.constant('DB_CONSTANTS', {
         "IS_NOT_EMPTY"  : "IS_NOT_NULL",
         "NULL_OR_EMPTY" : "NULL"
     },
-    "DATABASE_RANGE_MATCH_MODES": ["BETWEEN", "LESS_THAN", "LESS_THAN_OR_EQUALS", "GREATER_THAN", "GREATER_THAN_OR_EQUALS", "NOT_EQUALS"],
+    "DATABASE_RANGE_MATCH_MODES": ["IN", "NOT_IN", "BETWEEN", "LESS_THAN", "LESS_THAN_OR_EQUALS", "GREATER_THAN", "GREATER_THAN_OR_EQUALS", "NOT_EQUALS"],
     "ACTIONS": {
         "CREATE": "CREATE",
         "UPDATE": "UPDATE",
