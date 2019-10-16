9b264c3ed8388d7447d9761bd9460f13eafe6055
Sanjana <sanjana.raheja@wavemaker.com>
2019-10-16 19:48:40 +0530

    Issue: Logout functionality was breaking if roles were enabled for open ID provider. Cause: Casting exception(for SimpleGrantedAuthority to OidcUserAuthority) was observed when logout was triggered. Fix: Changing the authorities from SimpleGrantedAuthority type to OidcUserAuthority in the OpenIdUserService.java. Related to: RADX-308 - Access denied to the pages/services when a user is trying to access a page/service even though that user has given the access RADX-307 - Security info call response is not fetching the user roles properly
    Change-Id: I5212ded0c88742d566fa0246639ddbdf27e8f0ba

--- wavemaker-app-runtime-core/src/main/java/com/wavemaker/runtime/security/openId/OpenIdUserService.java
+++ wavemaker-app-runtime-core/src/main/java/com/wavemaker/runtime/security/openId/OpenIdUserService.java
@@ -1,8 +1,6 @@
 package com.wavemaker.runtime.security.openId;
 
-import java.util.HashSet;
-import java.util.List;
-
+import com.wavemaker.runtime.security.core.AuthoritiesProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
@@ -10,9 +8,12 @@ import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.security.oauth2.core.oidc.user.OidcUserAuthority;
 import org.springframework.util.CollectionUtils;
 
-import com.wavemaker.runtime.security.core.AuthoritiesProvider;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 
 /**
  * Created by srujant on 8/8/18.
@@ -29,12 +30,16 @@ public class OpenIdUserService extends OidcUserService {
             OpenIdAuthenticationContext openIdAuthenticationContext = new OpenIdAuthenticationContext(oidcUser.getName(), oidcUser);
             List<GrantedAuthority> grantedAuthorities = authoritiesProvider.loadAuthorities(openIdAuthenticationContext);
             if (!CollectionUtils.isEmpty(grantedAuthorities)) {
+                List<GrantedAuthority> updatedGrantedAuthorities = new ArrayList();
+                for (GrantedAuthority grantedAuthority : grantedAuthorities) {
+                    updatedGrantedAuthorities.add(new OidcUserAuthority(grantedAuthority.getAuthority(), oidcUser.getIdToken(), oidcUser.getUserInfo()));
+                }
                 String userNameAttributeName = userRequest.getClientRegistration()
                         .getProviderDetails().getUserInfoEndpoint().getUserNameAttributeName();
                 if (org.springframework.util.StringUtils.hasText(userNameAttributeName)) {
-                    oidcUser = new DefaultOidcUser(new HashSet<>(grantedAuthorities), oidcUser.getIdToken(), oidcUser.getUserInfo(), userNameAttributeName);
+                    oidcUser = new DefaultOidcUser(new HashSet<>(updatedGrantedAuthorities), oidcUser.getIdToken(), oidcUser.getUserInfo(), userNameAttributeName);
                 } else {
-                    oidcUser = new DefaultOidcUser(new HashSet<>(grantedAuthorities), userRequest.getIdToken(), oidcUser.getUserInfo());
+                    oidcUser = new DefaultOidcUser(new HashSet<>(updatedGrantedAuthorities), userRequest.getIdToken(), oidcUser.getUserInfo());
                 }
             }
         }
