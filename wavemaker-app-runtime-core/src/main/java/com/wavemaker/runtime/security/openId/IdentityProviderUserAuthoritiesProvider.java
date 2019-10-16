65e7996ae6fdda43567f5595a68dcfb895441ec2
Naresh <naresh.ravulapalli@wavemaker.com>
2019-10-16 13:20:40 +0530

    1. Added logger statements in the class. 2. In case the role attribute value was being returned as String type, a null value was being passed to the createAuthorities method causing application breakages at runtime. Fixed this scenario. 3. In case the roles are of none of the types mentioned in the conditions, then it would type case the roles into String and return it to the end user as it is.
    Related to:
    RADX-308 - Access denied to the pages/services when a user is trying to access a page/service even though that user has given the access
    RADX-307 - Security info call response is not fetching the user roles properly
    
    Change-Id: Ia8503bbc40a2c34cf57cdb7940becd685e5c1a68

--- wavemaker-app-runtime-core/src/main/java/com/wavemaker/runtime/security/openId/IdentityProviderUserAuthoritiesProvider.java
+++ wavemaker-app-runtime-core/src/main/java/com/wavemaker/runtime/security/openId/IdentityProviderUserAuthoritiesProvider.java
@@ -7,6 +7,8 @@ import java.util.Map;
 import java.util.StringTokenizer;
 
 import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
@@ -21,30 +23,36 @@ import com.wavemaker.runtime.security.core.AuthoritiesProvider;
  */
 public class IdentityProviderUserAuthoritiesProvider implements AuthoritiesProvider {
 
+    private static final Logger logger = LoggerFactory.getLogger(IdentityProviderUserAuthoritiesProvider.class);
     private String roleAttributeName;
 
     private static GrantedAuthoritiesMapper authoritiesMapper = new SimpleAuthorityMapper();
 
     @Override
     public List<GrantedAuthority> loadAuthorities(AuthenticationContext authenticationContext) {
+        logger.debug("In the loadAuthorities method to set the roles and permissions");
         OpenIdAuthenticationContext openIdAuthenticationContext = (OpenIdAuthenticationContext) authenticationContext;
         OidcUser oidcUser = openIdAuthenticationContext.getOidcUser();
 
         if (StringUtils.isNotBlank(roleAttributeName)) {
             Map<String, Object> claims = oidcUser.getClaims();
             Object roles = claims.get(roleAttributeName);
+            logger.debug("RoleAttributeName is {} with the roles as {}", roleAttributeName, roles);
             if (roles != null) {
                 String[] rolesArray = null;
                 if (roles instanceof String) {
                     List<String> rolesList = getRolesList((String) roles);
-                    rolesArray = new String[rolesList.size()];
+                    rolesArray = convertListToArray(rolesList);
                 } else if (roles instanceof String[]) {
                     rolesArray = (String[]) roles;
                 } else if (roles instanceof List) {
-                    List<String> rolesList = (List) roles;
-                    Object[] rolesObject = rolesList.toArray();
-                    rolesArray = Arrays.copyOf(rolesObject, rolesObject.length,String[].class);
+                    List rolesList = (List) roles;
+                    rolesArray = convertListToArray(rolesList);
+                } else {
+                    String rolesList = roles.toString();
+                    rolesArray = rolesList.split(",");
                 }
+                logger.debug("Roles {} are assigned", rolesArray);
                 List<GrantedAuthority> authorities =  AuthorityUtils.createAuthorityList(rolesArray);
                 authorities = new ArrayList(authoritiesMapper.mapAuthorities(authorities));
                 return authorities;
@@ -61,6 +69,9 @@ public class IdentityProviderUserAuthoritiesProvider implements AuthoritiesProvi
         this.roleAttributeName = roleAttributeName;
     }
 
+    private String[] convertListToArray(List<String> rolesList) {
+        return rolesList.toArray(new String[0]);
+    }
 
     private List<String> getRolesList(String roles) {
         StringTokenizer roleTokenizer = new StringTokenizer(roles, ",");
